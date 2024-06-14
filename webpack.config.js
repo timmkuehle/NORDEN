69992e486e7f8ed9201d3d6dcb1c6a6b015ce2f3
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-06-14 19:32:17 +0200

    Went back to specifically including each post type's styles and scripts in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -51,8 +51,10 @@ export default (env, argv) => {
 			.sync([
 				"./content/assets/src/scripts/index.@(js|ts)",
 				"./content/assets/src/styles/index.@(css|scss|sass)",
-				"./content/*/*/assets/src/scripts/page.@(js|ts)",
-				"./content/*/*/assets/src/styles/page.@(css|scss|sass)",
+				"./content/pages/*/assets/src/scripts/page.@(js|ts)",
+				"./content/pages/*/assets/src/styles/page.@(css|scss|sass)",
+				"./content/projects/*/assets/src/scripts/project.@(js|ts)",
+				"./content/projects/*/assets/src/styles/project.@(css|scss|sass)",
 				"./core/error/assets/src/scripts/error.@(js|ts)",
 				"./core/error/assets/src/styles/error.@(css|scss|sass)"
 			])
