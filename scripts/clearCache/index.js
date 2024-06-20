85e2031cd02ce9a2b9da4e6b951b3964fd8cd4cc
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-06-20 10:33:03 +0200

    Added script to clear cache

---
+++ scripts/clearCache/index.js
@@ -0,0 +1,15 @@
+import { rimrafSync } from "rimraf";
+import { logError, logStart, logSuccess } from "./logFunctions.js";
+import { getCacheDir } from "./directory.js";
+
+logStart();
+
+const cacheDir = getCacheDir();
+
+try {
+	rimrafSync(cacheDir.absolute);
+} catch (err) {
+	logError(err instanceof Error ? err.message : null);
+}
+
+logSuccess(cacheDir.relative);
