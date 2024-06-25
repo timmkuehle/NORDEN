3d679fefb6f7391196b9dbdba48bd069c17408c9
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-06-25 13:04:00 +0200

    Now calling JS font loader in Soelden project

--- content/projects/soelden/assets/src/scripts/project.ts
+++ content/projects/soelden/assets/src/scripts/project.ts
@@ -1,13 +1,4 @@
-const fontStyleTag = document.createElement("style");
-fontStyleTag.innerHTML =
-	'@font-face{font-family:"AS Soelden Headline";src:url("/content/resources/fonts/ASSoeldenHL/ASSoeldenHLWEB-HeavyFlex.woff")format("woff");font-weight:700;font-style:normal;font-display:swap;}@font-face{font-family:"AS Soelden";src:url("/content/resources/fonts/ASSoelden/ASSoelden-Regular.woff")format("woff");font-weight:400;font-style:normal;font-display:swap;}@font-face{font-family:"AS Soelden";src:url("/content/resources/fonts/ASSoelden/ASSoelden-Bold.woff")format("woff");font-weight:700;font-style: normal;font-display:swap;}';
-if ("requestIdleCallback" in window) {
-	requestIdleCallback(() => {
-		console.log("requestIdleCallback");
-		document.head.append(fontStyleTag);
-	});
-} else {
-	setTimeout(() => {
-		document.head.append(fontStyleTag);
-	}, 0);
-}
+import loadFonts from "@scripts/components/fontLoader";
+import fonts from "./fonts";
+
+loadFonts(fonts);
