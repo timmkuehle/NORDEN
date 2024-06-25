6e35b2b6e75fe2a346a327c8426b295d7415fbd8
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-06-25 13:17:20 +0200

    Added JS implementation of base fonts

---
+++ content/assets/src/scripts/constants/baseFonts.ts
@@ -0,0 +1,22 @@
+import { FontFace } from "@scripts/types";
+
+const fonts: FontFace[] = [
+	{
+		fontFamily: "Relevant",
+		filePath: "/content/resources/fonts/Relevant/Relevant-Normal.woff2",
+		format: "woff2",
+		fontWeight: 400,
+		ascentOverride: "95%",
+		descentOverride: "20%"
+	},
+	{
+		fontFamily: "Relevant",
+		filePath: "/content/resources/fonts/Relevant/Relevant-Black.woff2",
+		format: "woff2",
+		fontWeight: 700,
+		ascentOverride: "95%",
+		descentOverride: "20%"
+	}
+];
+
+export default fonts;
