618148a4f6a49c5588baeff8f0c7d7800c3a91d4
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-06-28 15:00:29 +0200

    headerAnimator no longer animates SVG logo viewBox

--- content/assets/src/scripts/components/headerAnimator.ts
+++ content/assets/src/scripts/components/headerAnimator.ts
@@ -1,5 +1,3 @@
-import gsap from "gsap";
-
 const setupHeaderAnimation = () => {
 	const siteHeader = document.querySelector("header.site-header");
 	const siteLogo = siteHeader?.querySelector(".logo");
@@ -19,23 +17,10 @@ const setupHeaderAnimation = () => {
 				siteHeader.classList.add("hidden");
 
 				if (!siteLogo || !(siteLogo instanceof SVGElement)) return;
-
-				gsap.to(siteLogo, {
-					attr: { viewBox: "0 0 43 45.1" },
-					duration: 0.3,
-					ease: "power3.inOut"
-				});
 			} else {
 				siteHeader.classList.remove("hidden");
 
 				if (!siteLogo || !(siteLogo instanceof SVGElement)) return;
-
-				gsap.to(siteLogo, {
-					attr: { viewBox: "0 0 256 45.1" },
-					duration: 0.3,
-					ease: "power3.inOut",
-					delay: 0.08
-				});
 			}
 
 			lastScrollTop = curScrollTop;
