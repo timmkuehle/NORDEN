13621be28ee1a26f495b1fb799a92e03576a6ff6
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-06-28 11:12:00 +0200

    MenuView now sets "active" class for active menu item

--- views/MenuView.php
+++ views/MenuView.php
@@ -24,6 +24,24 @@ class MenuView {
 	}
 
 	/**
+	 * Method to render menu item classnames
+	 *
+	 * @param string $uri Menu item URI
+	 */
+	private function renderMenuItemClassName(string $uri) {
+		$current_route = Router::getCurrentRoute('uri');
+		$class_name = 'menu-item';
+
+		if ($uri === $current_route) {
+			$class_name .= ' active-menu-item';
+		} elseif (str_contains($current_route, $uri)) {
+			$class_name .= ' has-active-menu-item';
+		}
+
+		echo $class_name;
+	}
+
+	/**
 	 * Render method
 	 *
 	 * Renders requested menu
@@ -32,7 +50,7 @@ class MenuView {
 		?>
         <ul class="nav-menu">
 		<?php foreach ($this->menu as $menu_item): ?>
-			<li class="menu-item">
+			<li class="<?php $this->renderMenuItemClassName($menu_item['link']); ?>">
 				<a class="menu-item-link" href="<?php echo BASE_URL .
     	$menu_item['link']; ?>"><?php echo $menu_item['title']; ?></a>
 			</li>	
