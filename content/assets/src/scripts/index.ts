68f97c9ffd7c19c09d4f36cf3530025140e8891d
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-06-28 14:12:02 +0200

    Adjusted index script to reflect headerAnimator name change

--- content/assets/src/scripts/index.ts
+++ content/assets/src/scripts/index.ts
@@ -3,7 +3,7 @@ import baseFonts from "./constants/baseFonts";
 import loadVideos from "./components/videoLoader";
 import setupLazyLoader from "./components/lazyLoader";
 import setupVideoAutoPlayer from "./components/videoAutoPlayer";
-import setupHeaderStateSetter from "./components/headerStateSetter";
+import setupHeaderAnimation from "./components/headerAnimator";
 
 if (!/\/projekte\/[^?&]+/.test(window.location.href)) {
 	loadFonts(baseFonts);
@@ -13,5 +13,5 @@ document.addEventListener("DOMContentLoaded", () => {
 	loadVideos();
 	setupLazyLoader();
 	setupVideoAutoPlayer();
-	setupHeaderStateSetter();
+	setupHeaderAnimation();
 });
