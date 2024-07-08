3ab37131a8d1bdae36cc7e0d9d01211e6a102a31
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-08 19:23:55 +0200

    Global relative base directory property is now set in index script

--- content/assets/src/scripts/index.ts
+++ content/assets/src/scripts/index.ts
@@ -4,6 +4,9 @@ import loadVideos from "./components/videoLoader";
 import setupLazyLoader from "./components/lazyLoader";
 import setupVideoAutoPlayer from "./components/videoAutoPlayer";
 import setupHeaderAnimation from "./components/headerAnimator";
+import { setRelativeBaseDir } from "./components/baseDirSetter";
+
+setRelativeBaseDir();
 
 if (!/\/projekte\/[^?&]+/.test(window.location.href)) {
 	loadFonts(baseFonts);
