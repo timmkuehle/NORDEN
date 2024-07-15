a1c9bc25276d12ce6c2076de66c96b2c71c1bfa9
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-15 14:11:43 +0200

    Added ARANEA project assets and route

--- config/routes.php
+++ config/routes.php
@@ -12,6 +12,8 @@ Router::addRoute('/kontakt', 'PageController');
 
 Router::addRoute('/projekte/soelden', 'ProjectController');
 
+Router::addRoute('/projekte/aranea', 'ProjectController');
+
 Router::addRoute('/impressum', 'PageController');
 
 Router::addRoute('/datenschutz', 'PageController');
