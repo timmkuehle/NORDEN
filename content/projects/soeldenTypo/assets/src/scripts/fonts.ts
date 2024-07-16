43e04395d49ba2e84e3c295e15ee9dff2b852240
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-16 13:57:42 +0200

    Added Soelden typo project assets

---
+++ content/projects/soeldenTypo/assets/src/scripts/fonts.ts
@@ -0,0 +1,25 @@
+import { FontFace } from "@scripts/types";
+
+const fonts: FontFace[] = [
+	{
+		fontFamily: "AS Soelden Headline",
+		filePath:
+			"/content/resources/fonts/ASSoeldenHL/ASSoeldenHLWEB-HeavyFlex.woff",
+		format: "woff",
+		fontWeight: 700
+	},
+	{
+		fontFamily: "AS Soelden",
+		filePath: "/content/resources/fonts/ASSoelden/ASSoelden-Regular.woff",
+		format: "woff",
+		fontWeight: 400
+	},
+	{
+		fontFamily: "AS Soelden",
+		filePath: "/content/resources/fonts/ASSoelden/ASSoelden-Bold.woff",
+		format: "woff",
+		fontWeight: 700
+	}
+];
+
+export default fonts;
