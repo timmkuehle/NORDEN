bbe49a8245ae9dbbb35ed600061962c8725be61c
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-16 03:32:19 +0200

    Finished ARANEA project template

--- content/projects/aranea/template.php
+++ content/projects/aranea/template.php
@@ -1,1 +1,371 @@
-<h1>Hallo<h1>
+<div class="aranea-intro bg-col-dark-green">
+	<?php new Svg(
+ 	'aranea-logo',
+ 	null,
+ 	'/content/resources/media/aranea/01_ARANEA_Logo.svg',
+ 	'ARANEA Logo'
+ ); ?>
+	<?php new Image(
+ 	'aranea-intro-image',
+ 	null,
+ 	'content/resources/media/aranea/02_ARANEA_Startbild.jpg',
+ 	'ARANEA Mood-Bild'
+ ); ?>
+</div>
+<section class="project-section bg-col-dark-green">
+	<div class="slot start">
+		<p class="side-note">What we stand for</p>
+	</div>
+	<div class="content">
+		<div class="column">
+		<?php new Svg(
+  	'aranea-claim',
+  	null,
+  	'/content/resources/media/aranea/03_ARANEA_Starttext.svg',
+  	'ARANEA stands for empowerment and support for girls* and young woman'
+  ); ?>
+		</div>
+		<div class="column col-light-green">
+			<p class="uppercase bold">ARANEA provides a space for girls* and young women* where they experience appreciation, education, reflection on femininity, and breaking down patriarchal structures.</p>
+			<p class="uppercase bold">The organization aims to reach people of all genders and age groups, working towards societal change and antidiscrimination through awareness-building.</p>
+		</div>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section class="services bg-col-dark-green">
+	<p class="service">Markenstrategie</p>
+	<p class="service">Corporate Design</p>
+	<p class="service">Branding & Implementierung</p>
+	<p class="service">Online & Offline Kommunikationskonzept</p>
+	<p class="service">Brandclip</p>
+	<p class="service">Animationen</p>
+	<p class="service">Social Media</p>
+	<p class="service">Anpassung aller Events / Submarken</p>
+</section>
+<section class="project-section small-padding no-padding-bottom-mobile">
+	<div class="slot start">
+		<p class="side-note">Logo Entwicklung</p>
+	</div>
+	<div class="content"></div>
+	<div class="slot end"></div>
+</section>
+<section class="project-section small-padding no-padding-top">
+	<div class="slot start"></div>
+	<div class="content">
+		<div class="column">
+			<h2>Bold, Versatil & Unique</h2>
+			<p>That's the new logo for ARANEA which exudes a sense of freedom and flexibility.</p>
+			<p>With the letter "A" appearing three times in the name, it takes on a special significance and can even be used as a standalone design element. Crafted from the font, we modified the "A" to give the logo its own distinctive touch. We also made slight adjustments to the "E" ensuring every detail reflects ARANEA's individuality.</p>
+		</div>
+		<div class="column">
+			<?php new Image(
+   	'aranea-logo-mods',
+   	null,
+   	'/content/resources/media/aranea/ARANEA_Logo_Modifizierungen.png',
+   	'ARANEA Logo Modifizierungen',
+   	true
+   ); ?>
+		</div>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section class="project-section full-width-new">
+	<div class="slot start"></div>
+	<div class="content">
+		<?php new Svg(
+  	'aranea-logo-dev',
+  	null,
+  	'/content/resources/media/aranea/Logoentwicklung_2.svg',
+  	'ARANEA Logo-Entwicklung'
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section class="project-section full-width-new">
+	<div class="slot start"></div>
+	<div class="content">
+		<?php new Svg(
+  	'aranea-logo-variants',
+  	null,
+  	'/content/resources/media/aranea/ARANEA_LogoUebersicht.svg',
+  	'ARANEA Logo-Varianten'
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section class="project-section">
+	<div class="slot start"></div>
+	<div class="content">
+		<div class="column">
+			<h2>Female body types</h2>
+			<p class="bold">“Each individual woman's body demands to be accepted on its own terms.”</p>
+			<p>Abstract forms, deduced from the different female body types, are used for backgrounddesign elements.
+			These abstract graphics capture the spirit of ARANEA celebrating individuality and inclusivity.</p>
+		</div>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section class="project-section">
+	<div class="slot start">
+		<p class="side-note">Graphic elements</p>
+	</div>
+	<div class="content">
+		<?php new Svg(
+  	'aranea-graphic-elements-desktop',
+  	'desktop-only',
+  	'/content/resources/media/aranea/Gestaltungselemente_Web.svg',
+  	'ARANEA Grafikelemente'
+  ); ?>
+  		<?php new Svg(
+    	'aranea-graphic-elements-mobile',
+    	'mobile-only',
+    	'/content/resources/media/aranea/Gestaltungselemente_Mobile.svg',
+    	'Grafikelemente'
+    ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section class="project-section">
+	<div class="slot start"></div>
+	<div class="content" style="justify-content: flex-end;">
+		<div class="column">
+			<h2>Typeface</h2>
+			<p>We chose a unique headline font. The typeface offers an endless glyph set, and can be anything you want it to be.</p>
+			<p>To the playful headline font, we choose a sans-serif typeface designed for user interfaces.</p>
+		</div>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section id="aranea-typo-section" class="project-section no-padding-top">
+	<div class="slot start"></div>
+	<div class="content">
+		<?php new Svg(
+  	'aranea-logo-variants',
+  	null,
+  	'/content/resources/media/aranea/04_ARANEA_Typographie.svg',
+  	'ARANEA Typographie'
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section class="project-section small-padding">
+	<div class="slot start">
+		<p class="side-note">Logo Entwicklung</p>
+	</div>
+	<div class="content"></div>
+	<div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top">
+	<div class="slot start"></div>
+	<div class="content">
+		<div class="column">
+			<h2>Colors</h2>
+			<p>Abstract forms, deduced from the different female body types, are used for backgrounddesign elements.
+			These abstract graphics capture the spirit of ARANEA celebrating individuality and inclusivity.</p>
+		</div>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section class="project-section small-padding">
+	<div class="slot start"></div>
+	<div class="content">
+		<?php new Svg(
+  	'aranea-colors-desktop',
+  	'desktop-only',
+  	'/content/resources/media/aranea/ARANEA_Farben_Web.svg',
+  	'ARANEA Farben'
+  ); ?>
+  		<?php new Svg(
+    	'aranea-colors-mobile',
+    	'mobile-only',
+    	'/content/resources/media/aranea/ARANEA_Farben_Mobile.svg',
+    	'ARANEA Farben'
+    ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section class="project-section bg-col-cyan overhang-right">
+	<div class="slot start">
+		<p class="side-note">Briefpapier</p>
+	</div>
+	<div class="content">
+		<?php new Image(
+  	'aranea-documents',
+  	null,
+  	'/content/resources/media/aranea/01_ARANEA_Briefpapier.jpg',
+  	'ARANEA Mood-Bild',
+  	true
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section class="project-section bg-col-orange">
+	<div class="slot start">
+		<p class="side-note">Website</p>
+	</div>
+	<div class="content">
+		<?php new Image(
+  	'aranea-website',
+  	null,
+  	'/content/resources/media/aranea/02_ARANEA_Website.jpg',
+  	'ARANEA Website',
+  	true
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section id="aranea-social-media-section" class="project-section">
+	<div class="slot start">
+		<p class="side-note">Social Media</p>
+	</div>
+	<div class="content">
+		<?php new Image(
+  	null,
+  	null,
+  	'/content/resources/media/aranea/ARANEA_SocialMedia_Web.png',
+  	'ARANEA Social Media',
+  	true,
+  	'/content/resources/media/aranea/ARANEA_SocialMedia_Mobile.png'
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section id="aranea-flyer-section" class="project-section">
+	<div class="slot start">
+		<p class="side-note">Flyer</p>
+	</div>
+	<div class="content">
+		<?php new Image(
+  	null,
+  	null,
+  	'/content/resources/media/aranea/04_ARANEA_Flyer.png',
+  	'ARANEA Flyer',
+  	true
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section id="aranea-bag-section" class="project-section full-width-new">
+	<div class="slot start"></div>
+	<div class="content">
+		<?php new Image(
+  	null,
+  	null,
+  	'/content/resources/media/aranea/05_ARANEA_Bag.png',
+  	'ARANEA Flyer',
+  	true
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section id="aranea-sticker-section" class="project-section overhang-right">
+	<div class="slot start">
+		<p class="side-note">Sticker</p>
+	</div>
+	<div class="content">
+		<?php new Image(
+  	null,
+  	null,
+  	'/content/resources/media/aranea/06_ARANEA_Sticker.png',
+  	'ARANEA Flyer',
+  	true
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section id="aranea-hoodie-section" class="project-section">
+	<div class="slot start">
+		<p class="side-note">T-shirts / Hoodies</p>
+	</div>
+	<div class="content">
+		<?php new Image(
+  	null,
+  	null,
+  	'/content/resources/media/aranea/07_ARANEA_Hoodie.jpg',
+  	'ARANEA Flyer',
+  	true
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section id="aranea-shirt-section" class="project-section full-width-new">
+	<div class="slot start"></div>
+	<div class="content">
+		<?php new Image(
+  	null,
+  	null,
+  	'/content/resources/media/aranea/08_ARANEA_T-Shirt.jpg',
+  	'ARANEA Flyer',
+  	true
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
+<section id="aranea-agency-section" class="project-section bg-col-dark-green has-background-image large-padding-bottom">
+	<div class="slot start">
+		<p class="side-note">Frontage branding</p>
+	</div>
+	<div class="content">
+		<div class="image-slideshow">
+			<div class="controls-container">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="button prev">
+					<path d="M7.4 273.4C2.7 268.8 0 262.6 0 256s2.7-12.8 7.4-17.4l176-168c9.6-9.2 24.8-8.8 33.9 .8s8.8 24.8-.8 33.9L83.9 232 424 232c13.3 0 24 10.7 24 24s-10.7 24-24 24L83.9 280 216.6 406.6c9.6 9.2 9.9 24.3 .8 33.9s-24.3 9.9-33.9 .8l-176-168z"/>
+				</svg>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="button next">
+					<path d="M440.6 273.4c4.7-4.5 7.4-10.8 7.4-17.4s-2.7-12.8-7.4-17.4l-176-168c-9.6-9.2-24.8-8.8-33.9 .8s-8.8 24.8 .8 33.9L364.2 232 24 232c-13.3 0-24 10.7-24 24s10.7 24 24 24l340.1 0L231.4 406.6c-9.6 9.2-9.9 24.3-.8 33.9s24.3 9.9 33.9 .8l176-168z"/>
+				</svg>
+			</div>
+			<div class="images-container">
+				<?php new Image(
+    	null,
+    	null,
+    	'/content/resources/media/aranea/ARANEA_Fassade_1.jpg',
+    	'ARANEA Fassade 1',
+    	true
+    ); ?>
+				<?php new Image(
+    	null,
+    	null,
+    	'/content/resources/media/aranea/ARANEA_Fassade_2.jpg',
+    	'ARANEA Fassade 2',
+    	true
+    ); ?>
+				<?php new Image(
+    	null,
+    	null,
+    	'/content/resources/media/aranea/ARANEA_Fassade_3.jpg',
+    	'ARANEA Fassade 3',
+    	true
+    ); ?>
+				<?php new Image(
+    	null,
+    	null,
+    	'/content/resources/media/aranea/ARANEA_Fassade_4.jpg',
+    	'ARANEA Fassade 4',
+    	true
+    ); ?>
+			</div>
+		</div>
+	</div>
+	<div class="slot end"></div>
+	<?php new Svg(
+ 	null,
+ 	'background-image',
+ 	'/content/resources/media/aranea/ARANEA_Element_Einzeln.svg'
+ ); ?>
+</section>
+<section id="aranea-shirt-section-2" class="project-section large-padding-bottom bg-col-orange">
+	<div class="slot start">
+	<p class="side-note">T-Shirt</p>
+	</div>
+	<div class="content">
+		<?php new Image(
+  	null,
+  	null,
+  	'/content/resources/media/aranea/08_ARANEA_T-Shirt.jpg',
+  	'ARANEA Flyer',
+  	true
+  ); ?>
+	</div>
+	<div class="slot end"></div>
+</section>
