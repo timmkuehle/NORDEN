9086e99d67c9d9bcaf8238e7284abacf66ea3cec
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-19 15:21:35 +0200

    Added content to Oetztaler project

--- content/projects/oetztaler/template.php
+++ content/projects/oetztaler/template.php
@@ -1,1 +1,180 @@
-<h1>Hallo</h1>
+<section id="logo-section" class="project-section bg-col-anthrazit small-padding">
+    <div class="slot start"></div>
+    <div class="content">
+        <h1>
+            <?php new Svg(
+            	null,
+            	null,
+            	'/content/resources/media/oetztaler/OERM_Logo.svg',
+            	'Ötztaler Radmarathon'
+            ); ?>
+        </h1>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="outro-video-section" class="project-section bg-col-anthrazit small-padding">
+    <div class="slot start">
+        <p class="side-note">OUTRO ANIMATION</p>
+    </div>
+    <div class="content">
+        <?php new Video(
+        	'oetztaler-outro-video',
+        	'/content/resources/media/oetztaler/OETZT_OERM_Outro_Web.mp4',
+        	'16/9',
+        	'/content/resources/media/oetztaler/OETZT_OERM_Outro_Web.jpg',
+        	'Oetztaler Outro-Video',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false,
+        	'/content/resources/media/oetztaler/OETZT_OERM_Outro_Mobile.mp4',
+        	'1/1',
+        	'/content/resources/media/oetztaler/OETZT_OERM_Outro_Mobile.jpg'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="intro-text-section" class="project-section bg-col-anthrazit small-padding overhang-right no-padding-bottom">
+    <div class="slot start"></div>
+    <div class="content" style="align-items: flex-start;">
+        <div class="column">
+            <p class="text-large col-white">Der Ötztaler Radmarathon ist mit 227 km und 5.500 Höhenmetern das anspruchsvollste Amateur-Radrennen in Europa. NORDEN entwickelte das gesamte Erscheinungsbild des Großevents in Sölden neu.</p>
+            <?php new Svg(
+            	null,
+            	'style-icon',
+            	'/content/resources/media/oetztaler/OERM_Website_Grafik_02.svg'
+            ); ?>
+        </div>
+        <div class="column">
+            <?php new Image(
+            	'intro-graphic',
+            	null,
+            	'/content/resources/media/oetztaler/OERM_Website_Grafik_01.svg',
+            	'Grafikelement',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="services">
+	<p class="service">Markenstrategie</p>
+	<p class="service">Corporate Design</p>
+	<p class="service">Branding & Implementierung</p>
+	<p class="service">Online & Offline Kommunikationskonzept</p>
+	<p class="service">Kommunikationkonzept</p>
+	<p class="service">Fontdesign</p>
+	<p class="service">Brandclip</p>
+	<p class="service">Motion Design</p>
+	<p class="service">Social Media Konzept</p>
+	<p class="service">Anpassung aller Events / Submarken</p>
+</section>
+<section id="movement-section" class="project-section has-background-image">
+    <div class="slot start"></div>
+    <div class="content narrow" style="justify-content: flex-end; align-items: flex-start">
+        <div class="column">
+            <h2>Building movement</h2>
+            <p>Dynamisch skalierbare Grafikelemente betonen die Geschwindigkeit und Bewegungen der Sportler:innen des Ötztaler Radmarathons. Die Farbgebung orientiert sich dabei konsequent an den Corporate Colors und der jeweiligen Highlightfarbe des Jahres. </p>
+        </div>
+        <div class="column">
+            <h3 class="spacer-headline">...fast</h3>
+            <p>Die Elemente wurden nach dem „Baukastenprinzip“ entwickelt. Je nach Anwendung und Format werden die Grafiken individuell angeordnet und animiert, wodurch stets neue, lebendige Motive entstehen.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+    <?php new Svg(
+    	null,
+    	'background-image',
+    	'/content/resources/media/oetztaler/OERM_Website_Grafik_03.svg'
+    ); ?>
+</section>
+<section id="design-concept-section" class="project-section no-padding-top" style="align-items: center;">
+    <div class="slot start">
+        <p class="side-note">Gestaltungskonzept</p>
+    </div>
+    <div class="content narrow">
+        <?php new Image(
+        	'design-concept-image',
+        	null,
+        	'/content/resources/media/oetztaler/OETZ_Icon-Entwicklung.png',
+        	'Grafikelement',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top no-padding-bottom full-width-new bg-col-blue">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	'design-concept-image',
+        	null,
+        	'/content/resources/media/oetztaler/03_OERM_Trennerbild_extended_updated.png',
+        	'Trennerbild',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section small-padding full-width-new bg-col-blue">
+    <div class="slot start"></div>
+    <div class="content" style="padding-inline: var(--main-padding-inline); align-items: flex-start;">
+        <h2 class="col-anthrazit">Animationstoolbox</h2>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top bg-col-blue">
+    <div class="slot start">
+        <p class="side-note">Animationstoolbox</p>
+    </div>
+    <div class="content">
+        <?php new Video(
+        	'oetztaler-animation-toolbox-video',
+        	'/content/resources/media/oetztaler/OETZT_Animationstoolbox.mp4',
+        	'16/9',
+        	'/content/resources/media/oetztaler/OETZT_Animationstoolbox_Still.jpg',
+        	'Oetztaler Animation-Toolbox Video',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="slideshow-section" class="project-section bg-col-blue" style="align-items: flex-end;">
+    <div class="slot start">
+        <p class="side-note">Sujets 2023 & 2024</p>
+    </div>
+    <div class="content narrow">
+        <div class="image-slideshow">
+			<div class="controls-container">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="button prev">
+					<path d="M7.4 273.4C2.7 268.8 0 262.6 0 256s2.7-12.8 7.4-17.4l176-168c9.6-9.2 24.8-8.8 33.9 .8s8.8 24.8-.8 33.9L83.9 232 424 232c13.3 0 24 10.7 24 24s-10.7 24-24 24L83.9 280 216.6 406.6c9.6 9.2 9.9 24.3 .8 33.9s-24.3 9.9-33.9 .8l-176-168z"/>
+				</svg>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="button next">
+					<path d="M440.6 273.4c4.7-4.5 7.4-10.8 7.4-17.4s-2.7-12.8-7.4-17.4l-176-168c-9.6-9.2-24.8-8.8-33.9 .8s-8.8 24.8 .8 33.9L364.2 232 24 232c-13.3 0-24 10.7-24 24s10.7 24 24 24l340.1 0L231.4 406.6c-9.6 9.2-9.9 24.3-.8 33.9s24.3 9.9 33.9 .8l176-168z"/>
+				</svg>
+			</div>
+			<div class="images-container">
+				<?php new Image(
+    	null,
+    	null,
+    	'/content/resources/media/oetztaler/05_OERM_Sujet2023.jpg',
+    	'Oetztaler Sujet 2023',
+    	true
+    ); ?>
+				<?php new Image(
+    	null,
+    	null,
+    	'/content/resources/media/oetztaler/05_OERM_Sujet2024.jpg',
+    	'Oetztaler Sujet 2024',
+    	true
+    ); ?>
+			</div>
+		</div>
+    </div>
+    <div class="slot end"></div>
+</section>
