bdf5504294ffb08f85ecad5e793e42170ea746c8
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-19 15:19:42 +0200

    Added slideshow function to Oetztaler project main script

--- content/projects/oetztaler/assets/src/scripts/main.ts
+++ content/projects/oetztaler/assets/src/scripts/main.ts
@@ -1,4 +1,9 @@
 import loadFonts from "@scripts/components/fontLoader";
 import fonts from "./fonts";
+import setupImageSlideshows from "@scripts/components/imageSlideshow";
 
 loadFonts(fonts);
+
+document.addEventListener("DOMContentLoaded", () => {
+	setupImageSlideshows();
+});
