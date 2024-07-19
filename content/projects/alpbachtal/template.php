60dbac3dc1ce8124f65889ed0c7579f9da5a8bdb
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-19 20:50:53 +0200

    Added Alpbachtal project content

---
+++ content/projects/alpbachtal/template.php
@@ -0,0 +1,295 @@
+<section id="intro-video-section" class="project-section full-width-new no-padding-top">
+    <div class="slot start"></div>
+    <div class="content">
+        <?php new Video(
+        	'emf-header-video',
+        	'/content/resources/media/alpbachtal/ALPB_WebsiteHeader_16zu9.mp4',
+        	'16/9',
+        	'/content/resources/media/alpbachtal/ALPACH_Logoaniamtion_16zu9_Still.jpg',
+        	'Alpbachtal Intro Video',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false,
+        	'/content/resources/media/alpbachtal/ALPB_WebsiteHeader_1zu1.mp4',
+        	'1/1',
+        	'/content/resources/media/alpbachtal/ALPACH_Logoaniamtion_1zu1_Still.jpg'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="intro-text-section" class="project-section">
+    <div class="slot start"></div>
+    <div class="content" style="align-items: flex-end;">
+        <div class="column" style="width: 60%">
+            <h1>Alpbachtal — Corporate Design</h1>
+            <p class="text-large">Tradition und Moderne. Schroffer Fels und sanfte Hügellandschaft. Diese Gegensätze charakterisiert nicht nur das Alpbachtal im Allgemeinen, sondern gelten auch für das neue Corporate Design der Marke Alpbachtal.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="services">
+	<p class="service">Markenstrategie</p>
+	<p class="service">Corporate Design</p>
+	<p class="service">Branding & Implementierung</p>
+	<p class="service">Online & Offline Kommunikationskonzept</p>
+	<p class="service">Animationen</p>
+	<p class="service">Social Media</p>
+	<p class="service">Identity Workshops</p>
+</section>
+<section id="logo-text-section" class="project-section no-padding-bottom" style="align-items: flex-end;">
+    <div class="slot start">
+        <p class="side-note">Entstehungsprozess</p>
+    </div>
+    <div class="content">
+        <div class="column">
+            <h2>Entstehungsprozess und Logo-Entwicklung</h2>
+            <p class="text-large">Tradition und Moderne. Schroffer Fels und sanfte Hügellandschaft. Diese Gegensätze charakterisiert nicht nur das Alpbachtal im Allgemeinen, sondern gelten auch für das neue Corporate Design der Marke Alpbachtal.</p>
+        </div>
+        <div class="column">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/alpbachtal/ALPBA_Schilder_Typo.jpg',
+            	'Logo-Entwicklung Schilder',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top no-padding-bottom full-width-new" style="position: relative; transform: translateY(6vw); z-index: -1" aria-hidden="true">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile" style="transform: rotate(180deg);">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/alpbachtal/ALPBA_Gestaltungselement_Overlay_beige4.svg',
+        	'Gestaltungselement',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-light-beige no-padding-top" style="align-items: center;">
+    <div class="slot start">
+        <p class="side-note">Logo Redesign</p>
+    </div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/alpbachtal/ALPBA_LogoHerleitung_desktop.svg',
+        	'Logo-Entwicklung',
+        	true,
+        	'/content/resources/media/alpbachtal/ALPBA_LogoHerleitung_mobile.svg'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section small-padding bg-col-light-beige">
+    <div class="slot start">
+        <p class="side-note">Ortsbezeichnungen</p>
+    </div>
+    <div class="content" style="align-items: flex-start;">
+        <div class="column" style="display: flex; flex-direction: column; height: 100%; justify-content: space-between;">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/alpbachtal/ALPBA_Logo_Kramsach.svg',
+            	'Alpbachtal Kramsach',
+            	true
+            ); ?>
+            <p style="margin-top: 2rem;">Selbst wenn die vielen Ortschaften der Region für sich stehen, die Kombination aus Ortsname und Markenlogo signalisiert stets die Zusammengehörigkeit der Region.</p>
+        </div>
+        <div class="column">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/alpbachtal/ALPBA_Logo_Ortsbezeichnungen.svg',
+            	'Alpbachtal Ortslogos',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top no-padding-bottom full-width-new" style="align-items: flex-start; position: relative; z-index: 50; height: 0;" aria-hidden="true">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/alpbachtal/ALPBA_Gestaltungselement_Overlay_beige4.svg',
+        	'Gestaltungselement',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top full-width-new">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/alpbachtal/shutterstock_1679802922_extended.jpg',
+        	'Trennerbild',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-bottom">
+    <div class="slot start"></div>
+    <div class="content" style="align-items: flex-start;">
+        <h2>Farbwelt</h2>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section small-padding" style="align-items: flex-end;">
+    <div class="slot start">
+        <p class="side-note">Farben</p>
+    </div>
+    <div class="content">
+        <div class="column">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/alpbachtal/ALPBA_Farbwelt_Beige.png',
+            	'Alpbachtal Beige',
+            	true
+            ); ?>
+        </div>
+        <div class="column">
+            <p>Die Farben des Corporate Designs leiten sich aus der Landschaft der Region Alpbachtal ab. Die Markenfarbe ist ein warmer, heller Holzton. Die Akzent-farben werden stets in farblicher Anlehnung an das jeweilige Bildmotiv eingesetzt.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section small-padding" style="align-items: flex-end;">
+    <div class="slot start"></div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/alpbachtal/ALPBA_Gesamt_Farbwelt_Web.png',
+        	'Alpbachtal Farben',
+        	true,
+        	'/content/resources/media/alpbachtal/ALPBA_Gesamt_Farbwelt_Mobile.png'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top no-padding-bottom full-width-new" style="position: relative; transform: translateY(6vw); z-index: -1" aria-hidden="true">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile" style="transform: rotate(180deg);">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/alpbachtal/ALPBA_Gestaltungselement_Overlay_beige3.svg',
+        	'Gestaltungselement',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-mid-beige" style="align-items: flex-end;">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column">
+            <h2>Typografie</h2>
+        </div>
+        <div class="column">
+            <p>Die Hausschrift wird in 2 Schriftstilen verwendet. Eine eher traditionell, holzig und natürlich anmutende Variante wird mit einer cleanen, modernen Variante kombiniert, um die im Alpbachtal deutliche Synthese aus Tradition und Moderne widerzuspiegeln.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-mid-beige">
+    <div class="slot start"></div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/alpbachtal/ALPBA_Hausschrift_Desktop.svg',
+        	'Alpbachtal Hausschrift',
+        	true,
+        	'/content/resources/media/alpbachtal/ALPBA_Hausschrift_Mobile1.svg'
+        ); ?>
+        <?php new Image(
+        	null,
+        	'mobile-only',
+        	'/content/resources/media/alpbachtal/ALPBA_Hausschrift_Mobile2.svg',
+        	'Alpbachtal Hausschrift 2',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top no-padding-bottom full-width-new bg-col-light-beige" style="align-items: flex-start; position: relative; z-index: 50;" aria-hidden="true">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/alpbachtal/ALPBA_Gestaltungselement_Overlay_beige3.svg',
+        	'Gestaltungselement',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top small-padding full-width-new">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Video(
+        	'emf-header-video',
+        	'/content/resources/media/alpbachtal/ALPBA_Gestaltungsprinzip_Anima.mp4',
+        	'16/9',
+        	'/content/resources/media/alpbachtal/ALPBA_Gestaltungsprinzip_Anima_Still.jpg',
+        	'Alpbachtal Gestaltungselement',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="design-principle-section" class="project-section small-padding" style="align-items: flex-start;">
+    <div class="slot start">
+        <p class="side-note">Gestaltungsprinzip</p>
+    </div>
+    <div class="content">
+        <p>Durch das skalieren und übereinander verschieben wird die Bildmarke zum Gestaltungselement. In der Umsetzung entstehen so lichte Farbflächen, die als Träger für Typografie und Inhalte dienen. Das gleiche Prinzip kommt auch mit Bildern zur Anwendung. NORDEN hat damit ein ganzheitliches Gestaltungskonzept kreiert, welches sich konsequent aus der Formensprache der Logotypografie ableitet.</p>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top no-padding-bottom">
+    <div class="slot start"></div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/alpbachtal/ALPBA_Gestaltungsprinzip.jpg',
+        	'Gestaltungsprinzip',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section small-padding">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column">
+            <p>Subtraktives Prinzip: Die Bildmarke wird in weiß zweifach über das Bild gelegt. Diese Gestaltungsform „stanzt” somit das Bild aus.</p>
+        </div>
+        <div class="column">
+            <p>Additives Prinzip: Die Bildmarke wird zweifach mit einem Bildmotiv gefüllt und auf einem weißen Hintergrund platziert.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
