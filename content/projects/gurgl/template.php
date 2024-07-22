a9b43ee44c976fbbe40edf0c3515bbbdf2f1b492
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-22 18:21:41 +0200

    Added additional Gurgl project contents

--- content/projects/gurgl/template.php
+++ content/projects/gurgl/template.php
@@ -155,12 +155,291 @@
             <?php new Button(null, null, 'Mehr zur Typografie', '/'); ?>
         </div>
         <div class="column">
-            <?php new Svg(
+            <?php new Image(
             	null,
             	null,
             	'/content/resources/media/gurgl/GURGL_Typo_Detail.svg'
             ); ?>
         </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="font-development-section-2" class="project-section no-padding-top">
+    <div class="slot start"></div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/gurgl/GURGL_Typo_Modifikation_Desktop.svg',
+        	'Gurgl Typografie-Modifikationen',
+        	true,
+        	'/content/resources/media/gurgl/GURGL_Typo_Modifikation_Mobile.svg'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="claim-headline-section" class="project-section has-background-image bg-col-gray-5">
+    <div class="slot start"></div>
+    <div class="content">
+        <?php new Svg(
+        	null,
+        	null,
+        	'/content/resources/media/gurgl/GURGL_Claim_headline.svg',
+        	'Gurgl. Diamond of the Alps'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+    <?php new Image(
+    	null,
+    	'background-image',
+    	'/content/resources/media/gurgl/Gurgl_Hintergrundelement_hell.svg',
+    	'Gestaltungselement'
+    ); ?>
+</section>
+<section id="colors-section" class="project-section has-background-image" style="align-items: center;">
+    <div class="slot start">
+        <p class="side-note">Colors</p>
+    </div>
+    <div class="content" style="align-items: flex-start;">
+        <div class="column">
+            <h2 class="col-white">Corporate colors</h2>
+            <h3 class="col-gray-1">Primary colors</h3>
+            <p class="col-white">Ein feiner und eleganter Grauton bildet die Primärfarbe des neuen Corporate Designs. Die spiegelt sowohl die</p>
+            <h3 class="col-gray-1">Secondary colors</h3>
+            <p class="col-white">Ausghend von der Primärfarbe wurden weitere Abstufungen 
+            definiert, um maximale Flexibilität in allen Anwendungen zu gewährleisten.</p>
+            <h3 class="col-gray-1">Tertiary colors</h3>
+            <p class="col-white">Der knallige Blauton wird bei Hervorhebungen, sowie Call-To-Action Anwendugen  eingesetzt und garantiert  größstmöglichen Kontrast und Aufmerksamkeit.</p>
+        </div>
+        <div class="column">
+            <?php new Svg(
+            	null,
+            	null,
+            	'/content/resources/media/gurgl/GURGL_Farben.svg',
+            	'GURGL Farben'
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+    <?php new Image(
+    	null,
+    	'background-image',
+    	'/content/resources/media/gurgl/Gurgl_Brandclip_Diamant.svg',
+    	'Gestaltungselement'
+    ); ?>
+</section>
+<section id="animation-section" class="project-section" style="align-items: flex-end;">
+    <div class="slot start">
+        <p class="side-note">Sommerkampagne</p>
+    </div>
+    <div class="content" style="flex-direction: column; align-items: flex-start">
+        <h2 class="col-white">Overlay Animationen</h2>
+        <?php new Video(
+        	null,
+        	'/content/resources/media/gurgl/Gurgl_Sommerkampagne_Web.mp4',
+        	'16/9',
+        	'/content/resources/media/gurgl/Gurgl_Sommerkampagne_Web_Still.jpg',
+        	'Gurgl Intro-Video',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false,
+        	'/content/resources/media/gurgl/Gurgl_Sommerkampagne_Mobile.mp4',
+        	'1/1',
+        	'/content/resources/media/gurgl/Gurgl_Sommerkampagne_Mobile_Still.jpg'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="some-section" class="project-section has-background-image" style="align-items: center;">
+    <div class="slot start">
+        <p class="side-note">Digital</p>
+    </div>
+    <div class="content" style="align-items: flex-start;">
+        <div class="column" style="width: 65%">
+            <h2 class="col-white">Social Media Toolbox</h2>
+            <p class="col-white">Um ein einheitliches Erscheinungsbild zu gewährleisten, haben wir ein allgemeingültiges Rastersystem entwickelt, welches sich proportional an die jeweilige Mediengröße anpasst.</p>
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/gurgl/GURGL_Digital_Story.png',
+            	'GURGL Social Media Story',
+            	true
+            ); ?>
+        </div>
+        <div class="column">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/gurgl/GURGL_Digital_Feed.png',
+            	'GURGL Social Media Feed',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+    <?php new Image(
+    	null,
+    	'background-image',
+    	'/content/resources/media/gurgl/Gurgl_Claim_Diamant.svg',
+    	'Gestaltungselement'
+    ); ?>
+</section>
+<section id="website-section" class="project-section">
+    <div class="slot start"></div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/gurgl/GURGL_Digital_Website.png',
+        	'Gurgl Website',
+        	false
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="magazine-section" class="project-section overhang-right has-background-image bg-col-gray-5" style="align-items: flex-end;">
+    <div class="slot start">
+        <p class="side-note">Print</p>
+    </div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/gurgl/GURGL_Print_Winterfolder.png',
+        	'Gurgl Website',
+        	false
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+    <?php new Image(
+    	null,
+    	'background-image',
+    	'/content/resources/media/gurgl/Gurgl_Hintergrundelement_hell.svg',
+    	'Gestaltungselement',
+    	true
+    ); ?>
+</section>
+<section id="standup-section" class="project-section overhang-right bg-col-gray-5">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column">
+            <h2 class="col-white">
+                <?php new Svg(
+                	null,
+                	null,
+                	'/content/resources/media/gurgl/GURGL_AlpineAuszeit_Headline.svg',
+                	'Alpine Auszeit'
+                ); ?>
+            </h2>
+            <p class="col-white">ci qui rerum assi omnis dolo volum eum quid quis et id et faciatis sam as eossuntist, te est et et rero issi aliquas eiunt. stia nat quosanistrum rem illuptiume sectiur? Ignam aut ommiquatem doluptatis es re inciant otatis est ut acepe cusam, c scon ni tem eario bla exped molorum qui nescips untibus,tausam doluptum rempos digenient voluptam et aped quis cum qui</p>
+        </div>
+        <div class="column">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/gurgl/GURGL_Print_AlpineArtzeit_Aufsteller.jpg',
+            	'Gurgl Aufsteller Alpine Auszeit',
+            	false
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="folder-section" class="project-section small-padding large-padding-bottom bg-col-gray-5 has-background-image">
+    <div class="slot start"></div>
+    <div class="content narrow">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/gurgl/GURGL_Print_AlpineArtzeit_Folder.png',
+        	'Gurgl Aufsteller Alpine Auszeit',
+        	false
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+    <?php new Image(
+    	null,
+    	'background-image',
+    	'/content/resources/media/gurgl/Gurgl_Hintergrundelement_hell.svg',
+    	'Gestaltungselement',
+    	true
+    ); ?>
+</section>
+<section id="worldcup-text-section" class="project-section">
+    <div class="slot start"></div>
+    <div class="content narrow" style="align-items: flex-end;">
+        <div class="column">
+            <h2>
+                <?php new Svg(
+                	null,
+                	null,
+                	'/content/resources/media/gurgl/GURGL_Skiweltcup_Headline.svg',
+                	'Ski Weltcup'
+                ); ?>
+            </h2>
+        </div>
+        <div class="column">
+            <h2 class="col-white">The Diamond of the Alps</h2>
+            <h3 class="col-white">AS Gurgl Headline Font:</h3>
+            <p class="col-white">Plakativ, präsent & sportlich. Die modifizierte Headline Font ist auffällig, athletisch-dynamisch und garantiert einen hohen Wiedererkennungsation</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section">
+    <div class="slot start"></div>
+    <div class="content narrow">
+        <div class="column" style="width: 36%;">
+            <?php new Video(
+            	null,
+            	'/content/resources/media/gurgl/Skiweltcup_Animiert.mp4',
+            	'9/16',
+            	'/content/resources/media/gurgl/Skiweltcup_Animiert_Still.png',
+            	'Ski-Weltcup-Animation',
+            	true,
+            	true,
+            	true,
+            	true,
+            	false
+            ); ?>
+        </div>
+        <div class="column">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/gurgl/GURGL_Skiweltcup_2023_DINA3_hoch_neue_Sponsoren_screen2.jpg',
+            	'Ski-Weltcup Poster',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section small-padding">
+    <div class="slot start"></div>
+    <div class="content narrow">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/gurgl/GURGL_SWC_Citylight.jpg',
+        	'Ski-Weltcup Citylight',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section">
+    <div class="slot start"></div>
+    <div class="content narrow">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/gurgl/GURGL__SWC2023_Eventuebersicht_Mockup.jpg',
+        	'Ski-Weltcup Event-Übersicht',
+        	true
+        ); ?>
     </div>
     <div class="slot end"></div>
 </section>
