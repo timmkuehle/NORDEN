ac640f8c34219956e932bd2edd35755458ab2970
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-22 14:51:25 +0200

    Added Gurgl project template

---
+++ content/projects/gurgl/template.php
@@ -0,0 +1,166 @@
+<div id="intro-bg-image-container" class="bg-image-container">
+<section id="logo-section" class="project-section bg-col-anthrazit small-padding">
+    <div class="slot start"></div>
+    <div class="content">
+        <h1>
+            <?php new Svg(
+            	null,
+            	null,
+            	'/content/resources/media/gurgl/00_Gurgl_Logo.svg',
+            	'GURGL - Diamond of the Alps'
+            ); ?>
+        </h1>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="intro-video-section" class="project-section bg-col-anthrazit small-padding no-padding-top" style="align-items: flex-end;">
+    <div class="slot start">
+        <p class="side-note">Brandclip</p>
+    </div>
+    <div class="content">
+        <?php new Video(
+        	null,
+        	'/content/resources/media/gurgl/Gurgl_Brandclip_Web.mp4',
+        	'16/9',
+        	'/content/resources/media/gurgl/Gurgl_Brandclip_Web_Still.jpg',
+        	'Gurgl Intro-Video',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false,
+        	'/content/resources/media/gurgl/Gurgl_Brandclip_Mobile.mp4',
+        	'1/1',
+        	'/content/resources/media/gurgl/Gurgl_Brandclip_Mobile_Still.jpg'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<?php new Image(
+	null,
+	'bg-image',
+	'/content/resources/media/gurgl/Gurgl_Brandclip_Diamant.svg',
+	'Gestaltungselement'
+); ?>
+</div>
+<section id="intro-text-section" class="project-section large-padding-bottom has-background-image">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column" style="width: 80%;">
+            <p class="text-large">Gurgl, Ici qui rerum assi omnis dolo volum eum quid quis et id et faciatis sam as eossuntist, te est et et rero issi aliquas eiunt. Lestia nat quosanistrum rem illuptiume sectiur? Ignam aut ommiquatem doluptatis es re inciant otatis est ut acepe cusam, c scon ni tem eario bla exped molorum qui nescips untibus,tausam doluptum rempos digenient voluptam et aped quis cum qui</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+    <?php new Image(
+    	null,
+    	'background-image',
+    	'/content/resources/media/gurgl/Gurgl_Hintergrundelement_hell.svg',
+    	'Gestaltungselement',
+    	true
+    ); ?>
+</section>
+<section id="claim-section" class="project-section bg-col-gray-5 no-padding-top has-background-image">
+    <div class="slot start">
+        <p class="side-note">Claim</p>
+    </div>
+    <div class="content">
+        <?php new Video(
+        	null,
+        	'/content/resources/media/gurgl/Gurgl_Claim.mp4',
+        	'16/9',
+        	'/content/resources/media/gurgl/Gurgl_Claim_Still.jpg',
+        	'Gurgl Intro-Video',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+    <?php new Image(
+    	null,
+    	'background-image',
+    	'/content/resources/media/gurgl/Gurgl_Claim_Diamant.svg',
+    	'Gestaltungselement'
+    ); ?>
+</section>
+<section id="design-element-section" class="project-section bg-col-gray-5" style="align-items: center;">
+    <div class="slot start">
+        <p class="side-note">Gestaltungselement</p>
+    </div>
+    <div class="content" style="align-items: stretch;">
+        <div class="column" style="display: flex; flex-direction: column; justify-content: space-between;">
+            <div class="text">
+                <h2 class="col-white">Gestaltungselement</h2>
+                <h3 class="col-gray-3">Gurgl. Der Diamant der Alpen</h3>
+                <p class="col-white">ci qui rerum assi omnis dolo volum eum quid quis et id et faciatis sam as eossuntist, te est et et rero issi aliquas eiunt. Lestia nat quosanistrum rem illuptiume sectiur? Ignam aut ommiquatem doluptatis es re inciant otatis est ut acepe cusam, c scon ni tem eario bla exped molorum qui nescips untibus,tausam doluptum rempos digenient voluptam et aped quis cum qui</p>
+            </div>
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/gurgl/GURGL_Gestaltungselement_Ausschnitt.png',
+            	'Gestaltungselement',
+            	true
+            ); ?>
+        </div>
+        <div class="column">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/gurgl/GURGL_Gestaltungselement_1.png',
+            	'Gestaltungselement',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="logo-animation-section" class="project-section bg-col-gray-5 has-background-image">
+    <div class="slot start">
+        <p class="side-note">Logo Animation</p>
+    </div>
+    <div class="content">
+        <?php new Video(
+        	null,
+        	'/content/resources/media/gurgl/Gurgl_Claim.mp4',
+        	'16/9',
+        	'/content/resources/media/gurgl/Gurgl_Claim_Still.jpg',
+        	'Gurgl Intro-Video',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+    <?php new Image(
+    	null,
+    	'background-image',
+    	'/content/resources/media/gurgl/Gurgl_Hintergrundelement_hell.svg',
+    	'Gestaltungselement',
+    	true
+    ); ?>
+</section>
+<section id="font-development-section" class="project-section">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column">
+            <h2>The diamond of the Alps</h2>
+            <h3 class="col-white">AS Gurgl headline font</h3>
+            <p>Plakativ, präsent & sportlich. Die modifizierte Headline Font ist auffällig, athletisch-dynamisch und garantiert einen hohen Wiedererkennungswert. Wir haben eine variable Headline-Font entwickelt, die Söldens prägnanten Charakter visualisiert. Verwendet wird sie ausschließlich bei den Headlines, bei allen Subbrands – vor allem im Bewegtbild entfaltet sie ihre volle Wirkung.</p>
+            <p>Die AS Sölden ist mit 10 unterschiedlichen Schriftschnitten ausgestattet. Wir bevorzugen die Schriftschnitte von Regular – Extrabold. Verwendet wird sie für Fließtexte und Sublines.</p>
+            <p>— Font creation in cooperation with <span class="col-blue">TypeTogether</span></p>
+            <?php new Button(null, null, 'Mehr zur Typografie', '/'); ?>
+        </div>
+        <div class="column">
+            <?php new Svg(
+            	null,
+            	null,
+            	'/content/resources/media/gurgl/GURGL_Typo_Detail.svg'
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
