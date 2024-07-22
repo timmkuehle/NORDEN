3e8c194d5a8eaad82934049c69064476b164319c
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-22 16:40:48 +0200

    Added Alpbachtal project fonts

--- content/projects/alpbachtal/assets/src/scripts/fonts.ts
+++ content/projects/alpbachtal/assets/src/scripts/fonts.ts
@@ -2,14 +2,14 @@ import { FontFace } from "@scripts/types";
 
 const fonts: FontFace[] = [
 	{
-		fontFamily: "AS Soelden",
-		filePath: "/content/resources/fonts/ASSoelden/ASSoelden-Regular.woff",
+		fontFamily: "Fedra Sans",
+		filePath: "/content/resources/fonts/Fedra/FedraSans-Book.woff2",
 		format: "woff",
 		fontWeight: 400
 	},
 	{
-		fontFamily: "AS Soelden",
-		filePath: "/content/resources/fonts/ASSoelden/ASSoelden-Bold.woff",
+		fontFamily: "Fedra Serif",
+		filePath: "/content/resources/fonts/Fedra/FedraSerif-Bold.woff2",
 		format: "woff",
 		fontWeight: 700
 	}
