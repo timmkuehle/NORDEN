7a9e284887ef16cc9f6e6859349cb011b45aafd6
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-23 12:00:06 +0200

    Added base fonts to Seefeld font script

--- content/projects/seefeld/assets/src/scripts/fonts.ts
+++ content/projects/seefeld/assets/src/scripts/fonts.ts
@@ -12,7 +12,25 @@ const fonts: FontFace[] = [
 		fontFamily: "Relevant",
 		filePath: "/content/resources/fonts/Relevant/Relevant-Light.woff2",
 		format: "woff2",
-		fontWeight: 300
+		fontWeight: 300,
+		ascentOverride: "95%",
+		descentOverride: "20%"
+	},
+	{
+		fontFamily: "Relevant",
+		filePath: "/content/resources/fonts/Relevant/Relevant-Normal.woff2",
+		format: "woff2",
+		fontWeight: 400,
+		ascentOverride: "95%",
+		descentOverride: "20%"
+	},
+	{
+		fontFamily: "Relevant",
+		filePath: "/content/resources/fonts/Relevant/Relevant-Black.woff2",
+		format: "woff2",
+		fontWeight: 700,
+		ascentOverride: "95%",
+		descentOverride: "20%"
 	}
 ];
 
