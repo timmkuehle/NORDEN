6adf1e8534cfdad27c29525a087dbddbbccd60b9
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-23 17:36:59 +0200

    Finished Seefeld project template

--- content/projects/seefeld/template.php
+++ content/projects/seefeld/template.php
@@ -56,3 +56,302 @@
     </div>
     <div class="slot end"></div>
 </section>
+<section class="project-section bg-col-light-blue no-padding-bottom">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column" style="width: 30%;">
+            <h2>Headline</h2>
+            <p>Pis quo comnis qui nus molestem nos ad unt liquunt isciet lab iuriberrum nobis aut quis et et aut voluptio. It doloriae quam fugiat reium exped ut repra coriatem</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-light-blue" style="align-items: flex-end;">
+    <div class="slot start">
+        <p class="side-note">Ortsbezeichnungen</p>
+    </div>
+    <div class="content" style="align-items: flex-start;">
+        <div class="column" style="width: 27%;">
+            <?php new Video(
+            	null,
+            	'/content/resources/media/seefeld/SFLD_Video_Fahnen.mp4',
+            	'9/16',
+            	'/content/resources/media/seefeld/SFLD_Video_Fahnen_Still.jpg',
+            	'Video Fahnen',
+            	true,
+            	true,
+            	true,
+            	true,
+            	false
+            ); ?>
+        </div>
+        <div class="column" style="width: 73%;">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/seefeld/SFLD_Ortsbezeichnungen_Desktop.svg',
+            	'Markenlogo und Ortsbezeichnungen',
+            	true,
+            	'content/resources/media/seefeld/SFLD_Ortsbezeichnungen_Mobile_1.svg'
+            ); ?>
+        </div>
+        <div class="column mobile-only">
+            <?php new Image(
+            	null,
+            	'mobile-only',
+            	'/content/resources/media/seefeld/SFLD_Ortsbezeichnungen_Mobile_2.svg',
+            	'Markenlogo und Ortsbezeichnungen - 2',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="font-section" class="project-section">
+    <div class="slot start">
+        <p class="side-note">Hausschrift</p>
+    </div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/seefeld/SFLD_Hausschriften_Desktop.svg',
+        	'Markenlogo und Ortsbezeichnungen',
+        	true,
+        	'/content/resources/media/seefeld/SFLD_Hausschriften_Mobile.svg'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section">
+    <div class="slot start">
+        <p class="side-note">Farben</p>
+    </div>
+    <div class="content" style="align-items: flex-end; gap: 3vw;">
+        <div class="column" style="width: 30%;">
+            <p style="text-align: right;">Die Farbwelt leitet sich aus der Natur um Seefeld ab. Kühle Blauabstufungen, kombiniert mit warmen Ockertönen runden die Farbwelt perfekt ab und bieten genügend Spielraum in allen Anwendungen.</p>
+        </div>
+        <div class="column" style="width: 70%;">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/seefeld/SFLD_Farbgebung.png',
+            	'Markenlogo und Ortsbezeichnungen',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="hochplateau-section" class="project-section full-width-new">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Svg(
+        	null,
+        	null,
+        	'/content/resources/media/seefeld/SFLD_Hochplateau_Headline.svg',
+        	'Hochplateau',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-light-blue" style="align-items: flex-start;">
+    <div class="slot start">
+        <p class="side-note">Gestaltungsprinzip</p>
+    </div>
+    <div class="content" style="align-items: flex-start;">
+        <div class="column" style="width: 30%;">
+            <p>Das Gestaltungsprinzip der Verschiebung von Farbfläche und Bildelementen lässt ein “grafisches Hochplateau” entstehen, und greift so die geografische Besonderheit der Region Seefeld subtil auf.</p>
+        </div>
+        <div class="column" style="width: 70%;">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/seefeld/SFLD_Gestaltungsprinzip.svg',
+            	'Gestaltungsprinzip',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="digital-section" class="project-section no-padding-bottom" style="align-items: flex-start;">
+    <div class="slot start">
+        <p class="side-note">Digital</p>
+    </div>
+    <div class="content" style="align-items: flex-start;">
+        <?php new Image(
+        	'seefeld-website',
+        	null,
+        	'/content/resources/media/seefeld/SFLD_Website.png',
+        	'Seefeld Website',
+        	true
+        ); ?>
+        <div id="digital-text">
+            <h2>Headline</h2>
+            <p class="col-white">Pis quo comnis qui nus molestem nos ad unt liquunt isciet lab iuriberrum nobis aut quis et et aut voluptio. It doloriae quam fugiat reium exped ut repra coriatem</p>
+        </div>
+        <?php new Image(
+        	'seefeld-social-media',
+        	null,
+        	'/content/resources/media/seefeld/SFLD_SocialMedia.png',
+        	'Seefeld Social Media',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top no-padding-bottom full-width-new" style="position: relative; z-index: -1;">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/seefeld/09_SFLD_Trennerbild.jpg',
+        	'Trennerbild',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top full-width-new">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/seefeld/10_SFLD_Hochplateau Trenner_Banner.gif',
+        	'Seefeld Banner',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section" style="align-items: center;">
+    <div class="slot start">
+        <p class="side-note">Print</p>
+    </div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/seefeld/SFLD_Print_Mockup.jpg',
+        	'Seefeld Flyer',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section full-width-new" style="align-items: center;">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/seefeld/02_SFLD_Mockup_Broschuere_Magazin.png',
+        	'Seefeld Print Mockups',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column">
+            <h2>Headline</h2>
+            <p>Pis quo comnis qui nus molestem nos ad unt liquunt isciet lab iuriberrum nobis aut quis et et aut voluptio. It doloriae quam fugiat reium exped ut repra coriatem</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="icons-section" class="project-section has-background-image small-padding large-padding-bottom" style="align-items: center;">
+    <div class="slot start">
+        <p class="side-note">Piktogramme</p>
+    </div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/seefeld/SFLD_icons_Website.svg',
+        	'Seefeld Flyer',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+    <?php new Image(
+    	null,
+    	'background-image',
+    	'/content/resources/media/seefeld/SFLD_icons_Website_Rodeln.svg',
+    	'Seefeld Flyer',
+    	true
+    ); ?>
+</section>
+<section id="papers-section" class="project-section bg-col-light-blue">
+    <div class="slot start"></div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/seefeld/SFLD_Geschaeftsausstattung.png',
+        	'Seefeld Geschäftsaustattung',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="branding-section-1" class="project-section" style="align-items: center;">
+    <div class="slot start">
+        <p class="side-note">Branding</p>
+    </div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/seefeld/13_SFLD_Branding_NordicCombinedTriple2023.jpg',
+        	'Branding Nordische Kombination',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="branding-section-2" class="project-section no-padding-top" style="align-items: center;">
+    <div class="slot start"></div>
+    <div class="content" style="align-items: flex-start;">
+        <div class="column">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/seefeld/13_SFLD_Branding_Running.jpg',
+            	'Branding Running',
+            	true
+            ); ?>
+            <h2>Headline</h2>
+            <p class="col-white">Pis quo comnis qui nus molestem nos ad unt liquunt isciet lab iuriberrum nobis aut quis et et aut voluptio. It doloriae quam fugiat reium exped ut repra coriatem</p>
+        </div>
+        <div class="column">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/seefeld/13_SFLD_Branding_WM_Halle.jpg',
+            	'Branding WM-Halle',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section full-width-new">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/seefeld/14_SFLD_Merchandising_gesamt.jpg',
+        	'Seefeld Flyer',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
