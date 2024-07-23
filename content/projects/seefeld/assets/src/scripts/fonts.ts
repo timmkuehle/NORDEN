f344fa5a504f00411650ac7189281633c3187af3
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-23 11:53:57 +0200

    Added Seefeld project scripts

---
+++ content/projects/seefeld/assets/src/scripts/fonts.ts
@@ -0,0 +1,19 @@
+import { FontFace } from "@scripts/types";
+
+const fonts: FontFace[] = [
+	{
+		fontFamily: "Kitsch Text",
+		filePath:
+			"/content/resources/fonts/KitschText/KitschText-Regular.woff2",
+		format: "woff2",
+		fontWeight: 400
+	},
+	{
+		fontFamily: "Relevant",
+		filePath: "/content/resources/fonts/Relevant/Relevant-Light.woff2",
+		format: "woff2",
+		fontWeight: 300
+	}
+];
+
+export default fonts;
