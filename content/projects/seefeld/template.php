6956971249686592f70204c4545217537ba0aa87
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-23 11:53:32 +0200

    Added initial Seefeld project template

---
+++ content/projects/seefeld/template.php
@@ -0,0 +1,58 @@
+<section id="logo-section" class="project-section no-padding-bottom" style="align-items: flex-end;">
+    <div class="slot start">
+        <p class="side-note">Logo</p>
+    </div>
+    <div class="content" style="flex-direction: column;">
+        <?php new Svg(
+        	'seefeld-logo',
+        	null,
+        	'/content/resources/media/seefeld/SFLD_Logo2022_4c_rz.svg',
+        	'Seefeld Logo'
+        ); ?>
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/seefeld/01_SFLD_Header.jpg',
+        	'Seefeld Header',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="intro-text-section" class="project-section">
+    <div class="slot start"></div>
+    <div class="content" style="justify-content: center;">
+        <div class="column" style="width: 70%;">
+            <p class="text-large" style="text-align: center;">Die Region Seefeld liegt auf einem Hochplateau zwischen Wettersteingebirge und Karwendel und ist für seine Langlaufloipen bekannt. 2021 wurde entschieden, sich von dem Namen “Olympiaregion Seefeld” zu trennen und die Region anhand von ihren geografischen Besonderheit – dem Hochplateau – zu beschreiben. Dieses Alleinstellungsmerkmal durften wir in einem Logorelaunch, sowie einem neuen Corporate Design grafisch interpretieren und umsetzten.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="services bg-col-dark-green">
+	<p class="service">Markenstrategie</p>
+	<p class="service">Corporate Design</p>
+	<p class="service">Piktogrammfamilie</p>
+	<p class="service">Logo Redesign</p>
+	<p class="service">Social Media Toolbox</p>
+	<p class="service">Kommunikationskonzept</p>
+</section>
+<section id="logo-redesign-section" class="project-section" style="align-items: center;">
+    <div class="slot start">
+        <p class="side-note">Logoredesign</p>
+    </div>
+    <div class="content">
+        <?php new Video(
+        	null,
+        	'/content/resources/media/seefeld/SFLD_Logoentwicklung_Animation.MP4',
+        	'16/9',
+        	'/content/resources/media/seefeld/SFLD_Logoentwicklung_Animation_Still.jpg',
+        	'Seefeld Header',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
