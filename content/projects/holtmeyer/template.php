965a42c905d540af03047f069e5da3800092d22c
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-24 14:06:17 +0200

    Finished Holtmeyer project template

--- content/projects/holtmeyer/template.php
+++ content/projects/holtmeyer/template.php
@@ -50,8 +50,8 @@
     </div>
     <div class="content">
         <div class="column">
-            <h2>Seit mehr als 100 Jahren auf dem Holzweg</h2>
-            <p class="text-large">Vor mehr als 100 Jahren als Sägewerk gegründet, vereint Holtmeyer heute unter dem Dach einer Holding drei Unternehmen: Sägewerk und Holzhandel, Pellets und Energie.           Diese gewachsene Dach-markenstrategie spiegelt sich auch im neuen Erscheinungsbild der Marke Holtmeyer wider.</p>
+            <h2 style="margin-left: 2vw;">Seit mehr als 100 Jahren auf dem Holzweg</h2>
+            <p style="margin-left: 2vw;">Vor mehr als 100 Jahren als Sägewerk gegründet, vereint Holtmeyer heute unter dem Dach einer Holding drei Unternehmen: Sägewerk und Holzhandel, Pellets und Energie. Diese gewachsene Dach-markenstrategie spiegelt sich auch im neuen Erscheinungsbild der Marke Holtmeyer wider.</p>
         </div>
     </div>
     <div class="slot end"></div>
@@ -62,9 +62,9 @@
     </div>
     <div class="content">
         <?php new Video(
-        	'emf-header-video',
+        	null,
         	'/content/resources/media/holtmeyer/HOLT_Animation_Desktop.mp4',
-        	'16/9',
+        	'1920/850',
         	'/content/resources/media/holtmeyer/HOLT_Animation_Desktop_STILL.png',
         	'HOLTMEYER Markenhierarchie',
         	true,
@@ -73,7 +73,7 @@
         	true,
         	false,
         	'/content/resources/media/holtmeyer/HOLT_Animation_Mobile.mp4',
-        	'1/1',
+        	'1200/850',
         	'/content/resources/media/holtmeyer/HOLT_Animation_Mobile_STILL.png'
         ); ?>
     </div>
@@ -91,6 +91,195 @@
         	'HOLTMEYER Farbwelt'
         ); ?>
         <p>Die Struktur der Dachmarke Holtmeyer offenbart sich nicht nur im Logo, sondern ist auch die Grundlage für die Farbgebung der einzelnen Marken. Die Farbpalette der Unternehmens-identität wird aus den spezifischen Wirkungsbereichen der Firmen abgeleitet, ohne dabei ein bestimmtes Tätigkeitsfeld übermäßig zu betonen.</p>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="font-section" class="project-section bg-col-beige large-padding-bottom large-padding-top">
+    <div class="slot start">
+        <p class="side-note">Typografie</p>
+    </div>
+    <div class="content">
+        <?php new Svg(
+        	null,
+        	'desktop-only',
+        	'/content/resources/media/holtmeyer/HOLT_Typo_Web.svg',
+        	'HOLTMEYER Hausschrift'
+        ); ?>
+        <?php new Svg(
+        	null,
+        	'mobile-only',
+        	'/content/resources/media/holtmeyer/HOLT_Typo_Mobile.svg',
+        	'HOLTMEYER Hausschrift'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section full-width-new no-padding-top no-padding-bottom">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/holtmeyer/06_HOLT_Trennerbild_Web.jpg',
+        	'Trennerbild',
+        	true,
+        	'/content/resources/media/holtmeyer/06_HOLT_Trennerbild_Mobile.jpg'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<div class="bg-col-brown">
+<section id="design-principle-section" class="project-section overhang-right">
+    <div class="slot start">
+        <p class="side-note">Gestaltungselement</p>
+    </div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/holtmeyer/HOLT_Gestaltungsprinzip_Desktop.svg',
+        	'HOLTMEYER Gestaltungselement',
+        	true
+        ); ?>
+        <p id="design-principle-text" class="col-white">Das Gestaltungselement der Holding wurde von einem Sägeblatt inspiriert, einem der wichtigsten Werkzeuge von Holtmeyer. Dieses wird oft prominent platziert, jedoch kann es sowohl schlicht als auch äußerst kreativ eingesetzt werden. Diese Vielseitigkeit erlaubt es, das Symbol auf verschiedenste Weise einzubinden und dem Design so eine besondere Note zu verleihen.</p>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="design-use-case-section" class="project-section no-padding-top">
+    <div class="slot start">
+        <p class="side-note">Geschäftsausstattung</p>
+    </div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/holtmeyer/08_HOLT_Briefpapier.png',
+        	'HOLTMEYER Briefpapier',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+</div>
+<section class="project-section full-width-new no-padding-top no-padding-bottom">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/holtmeyer/10_HOLT_LKW_Branding_Web.jpg',
+        	'HOLTMEYER LKW-Branding',
+        	true,
+        	'/content/resources/media/holtmeyer/10_HOLT_LKW_Branding_Mobile.jpg'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="font-section" class="project-section bg-col-beige no-padding-bottom">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column">
+            <p>Das Erscheinungsbild von Holtmeyer lässt sich mühelos auf alle Kanäle übertragen, sei es auf der Website oder in Social-Media-Beiträgen. Die Inhalte der Submarken werden dabei in einem schlichten Design präsentiert und behalten stets ihre charak-teristische Farbgebung bei.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="font-section" class="project-section bg-col-beige">
+    <div class="slot start">
+        <p class="side-note">Website + Social Media</p>
+    </div>
+    <div class="content" style="align-items: flex-start;">
+        <div class="column" style="width: 81%;">
+            <?php new Video(
+            	null,
+            	'/content/resources/media/holtmeyer/HOLT_Website_Screendesign.mp4',
+            	'1000/638',
+            	'/content/resources/media/holtmeyer/HOLT_Website_Screendesign_Still.png',
+            	'HOLTMEYER Website',
+            	true,
+            	true,
+            	true,
+            	true,
+            	false
+            ); ?>
+        </div>
+        <div class="column" style="width: 19%;">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/holtmeyer/12_HOLT_Stellenausschreibung.png',
+            	'HOLTMEYER Stellenausschreibung',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section full-width-new no-padding-top no-padding-bottom">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/holtmeyer/13_HOLT_Trennerbild_Web.jpg',
+        	'HOLTMEYER LKW-Branding',
+        	true,
+        	'/content/resources/media/holtmeyer/13_HOLT_Trennerbild_Mobile.jpg'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column" style="width: 40%;">
+            <h2 style="margin-left: 2vw;">Die Arbeit ruft!</h2>
+            <p style="margin-left: 2vw;">Um das Erscheinungsbild von Holtmeyer bis zu den Mitarbeiter:innen, dem Herzen des Unternehmens, zu tragen, wurde konsequente Arbeitskleidung für jeden Einsatzbereich des Unternehmens entwickelt.</p>
+            <p style="margin-left: 2vw;">Dazu gehören beispielsweise T-Shirts, Hosen und Warnwesten. So tragen alle Mitarbeiter:innen stolz die Identität des Unternehmens nach außen und zeigen Einheitlichkeit und Professionalität in jeder Situation.</p>
+        </div>
+        <div class="column" style="width: 60%;">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/holtmeyer/15_Honig_Tasse.jpg',
+            	'HOLTMEYER Stellenausschreibung'
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="merch-section" class="project-section bg-col-beige no-padding-bottom">
+    <div class="slot start"></div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/holtmeyer/15_Honig_Tasse.jpg',
+        	'HOLTMEYER Stellenausschreibung'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-beige">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column" style="width: 30%;">
+            <p>Für den alltäglichen Gebrauch bei Holtmeyer wurden Stempel, Zollstöcke, Notizblöcke und Bleistifte gebrandet. Anlässlich des 100-jährigen Jubiläums von Holtmeyer wurden jedoch auch spezielle Tassen produziert, welche die lange Traditionsgeschichte des Unternehmens zelebrieren.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section full-width-new no-padding-top">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/holtmeyer/17_HOLT_Trennerbild_Web.jpg',
+        	'Trennerbild',
+        	true,
+        	'/content/resources/media/holtmeyer/17_HOLT_Trennerbild_Mobile.jpg'
+        ); ?>
     </div>
     <div class="slot end"></div>
 </section>
