baf98bc869465c593b38cdff68240ed16bde4030
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-24 11:46:50 +0200

    Added Holtmeyer project fonts

---
+++ content/projects/holtmeyer/assets/src/scripts/fonts.ts
@@ -0,0 +1,20 @@
+import { FontFace } from "@scripts/types";
+
+const fonts: FontFace[] = [
+	{
+		fontFamily: "Officina Sans",
+		filePath:
+			"/content/resources/fonts/OfficinaSans/OfficinaSansITC-Book.ttf",
+		format: "truetype",
+		fontWeight: 400
+	},
+	{
+		fontFamily: "Officina Sans",
+		filePath:
+			"/content/resources/fonts/OfficinaSans/OfficinaSansITC-Bold.ttf",
+		format: "truetype",
+		fontWeight: 700
+	}
+];
+
+export default fonts;
