bae3b6d919a60664acf040c94a1717ff8b086650
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-24 12:23:58 +0200

    Added Holtmeyer project template

---
+++ content/projects/holtmeyer/template.php
@@ -0,0 +1,96 @@
+<section id="intro-video-section" class="project-section full-width-new no-padding-top no-padding-bottom">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Video(
+        	null,
+        	'/content/resources/media/holtmeyer/Holtmeyer_startseite_Web.mp4',
+        	'16/9',
+        	'/content/resources/media/holtmeyer/Holtmeyer_startseite_Web_Still.jpg',
+        	'HOLTMEYER Intro Video',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false,
+        	'/content/resources/media/holtmeyer/Holtmeyer_startseite_Mobile.mp4',
+        	'1/1',
+        	'/content/resources/media/holtmeyer/Holtmeyer_startseite_Mobile_Still.jpg'
+        ); ?>
+        <h1 id="page-title">
+            <?php new Svg(
+            	null,
+            	null,
+            	'/content/resources/media/holtmeyer/Holtmeyer_Holding_Logo_weiß.svg',
+            	'HOLTMEYER'
+            ); ?>
+        </h1>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="intro-text-section" class="project-section">
+    <div class="slot start"></div>
+    <div class="content" style="justify-content: flex-end;">
+        <div class="column">
+            <p class="text-large">Eine Marke, drei Unternehmen und unzählige Produkte. Jedes Unternehmen agiert eigenständig und dennoch arbeitet jeder Bereich nachhaltig und Hand in Hand als geschlossener Kreislauf.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="services">
+	<p class="service">Gestaltungskonzept</p>
+	<p class="service">Branding & Implementierung</p>
+	<p class="service">Online & Offline Kommunikationskonzept</p>
+	<p class="service">Motion Design</p>
+	<p class="service">Social Media Toolbox</p>
+	<p class="service">Merchandise</p>
+</section>
+<section id="logo-text-section" class="project-section">
+    <div class="slot start">
+        <p class="side-note">Logo</p>
+    </div>
+    <div class="content">
+        <div class="column">
+            <h2>Seit mehr als 100 Jahren auf dem Holzweg</h2>
+            <p class="text-large">Vor mehr als 100 Jahren als Sägewerk gegründet, vereint Holtmeyer heute unter dem Dach einer Holding drei Unternehmen: Sägewerk und Holzhandel, Pellets und Energie.           Diese gewachsene Dach-markenstrategie spiegelt sich auch im neuen Erscheinungsbild der Marke Holtmeyer wider.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="logo-animation-section" class="project-section">
+    <div class="slot start">
+        <p class="side-note">Markenhierarchie</p>
+    </div>
+    <div class="content">
+        <?php new Video(
+        	'emf-header-video',
+        	'/content/resources/media/holtmeyer/HOLT_Animation_Desktop.mp4',
+        	'16/9',
+        	'/content/resources/media/holtmeyer/HOLT_Animation_Desktop_STILL.png',
+        	'HOLTMEYER Markenhierarchie',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false,
+        	'/content/resources/media/holtmeyer/HOLT_Animation_Mobile.mp4',
+        	'1/1',
+        	'/content/resources/media/holtmeyer/HOLT_Animation_Mobile_STILL.png'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="colors-section" class="project-section">
+    <div class="slot start">
+        <p class="side-note">Farben</p>
+    </div>
+    <div class="content" style="flex-direction: column;">
+        <?php new Svg(
+        	null,
+        	null,
+        	'/content/resources/media/holtmeyer/04_HOLT_Farben.svg',
+        	'HOLTMEYER Farbwelt'
+        ); ?>
+        <p>Die Struktur der Dachmarke Holtmeyer offenbart sich nicht nur im Logo, sondern ist auch die Grundlage für die Farbgebung der einzelnen Marken. Die Farbpalette der Unternehmens-identität wird aus den spezifischen Wirkungsbereichen der Firmen abgeleitet, ohne dabei ein bestimmtes Tätigkeitsfeld übermäßig zu betonen.</p>
+    </div>
+    <div class="slot end"></div>
+</section>
