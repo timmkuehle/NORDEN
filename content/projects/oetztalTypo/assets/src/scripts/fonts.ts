422c55746165cf5c9d0620befae239a6485f92ff
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-25 15:22:50 +0200

    Added Oetztal Typo project fonts

---
+++ content/projects/oetztalTypo/assets/src/scripts/fonts.ts
@@ -0,0 +1,24 @@
+import { FontFace } from "@scripts/types";
+
+const fonts: FontFace[] = [
+	{
+		fontFamily: "AS Oetztal",
+		filePath: "/content/resources/fonts/ASOetztal/ASOetztal-Light.woff2",
+		format: "woff2",
+		fontWeight: 300
+	},
+	{
+		fontFamily: "AS Oetztal",
+		filePath: "/content/resources/fonts/ASOetztal/ASOetztal-Regular.woff2",
+		format: "woff2",
+		fontWeight: 400
+	},
+	{
+		fontFamily: "AS Oetztal",
+		filePath: "/content/resources/fonts/ASOetztal/ASOetztal-Bold.woff2",
+		format: "woff2",
+		fontWeight: 700
+	}
+];
+
+export default fonts;
