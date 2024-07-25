f522a8bf21d9e1dba6986844ca9aa0f19edce62c
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-25 14:08:50 +0200

    Finsished Test Fest project template

--- content/projects/testFest/template.php
+++ content/projects/testFest/template.php
@@ -1,11 +1,14 @@
 <section class="project-section full-width-new no-padding-top small-padding">
     <div class="slot start"></div>
     <div class="content no-inline-padding-mobile">
-        <?php new Image(
-        	null,
-        	null,
-        	'/content/resources/media/testFest/TestFest_Header.jpg'
-        ); ?>
+        <h1>
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/testFest/TestFest_Header.jpg',
+            	'Test Fest Sölden'
+            ); ?>
+        </h1>
     </div>
     <div class="slot end"></div>
 </section>
@@ -34,4 +37,225 @@
 	<p class="service">Branding & Implementierung</p>
 	<p class="service">Online & Offline Kommunikationskonzept</p>
 	<p class="service">Social Media</p>
+</section>
+<section class="project-section bg-col-anthrazit">
+    <div class="slot start">
+        <p class="side-note">Logo</p>
+    </div>
+    <div class="content" style="align-items: stretch;">
+        <div class="column" style="width: 70%;">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/testFest/TestFest_Logo_mitPfeil.svg',
+            	'Test Fest Logo mit Pfeil',
+            	true
+            ); ?>
+        </div>
+        <div class="column" style="display: flex; flex-direction: column; gap: 3vw; justify-content: space-between">
+            <div class="wrapper">
+                <h2 class="col-light-blue">Logo</h2>
+                <h3>Font:</h3>
+                <p class="col-white">Adelle Sans Sölden als verbindendes Corporate Element zum Sölden Auftritt.</p>
+            </div>
+            <div class="wrapper">
+                <h3>Flexibles Logosystem:</h3>
+                <?php new Image(
+                	null,
+                	null,
+                	'/content/resources/media/testFest/TestFest_Logo_System.svg',
+                	'Test Fest Logosystem',
+                	true
+                ); ?>
+            </div>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-anthrazit small-padding">
+    <div class="slot start">
+        <p class="side-note">Colors</p>
+    </div>
+    <div class="content"></div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-anthrazit no-padding-top">
+    <div class="slot start"></div>
+    <div class="content" style="align-items: flex-start;">
+        <div class="column" style="width: 30%;">
+            <h2 class="col-light-blue">Colors</h2>
+            <p class="col-white">Adelle Sans Sölden als verbindendes Corporate Element zum Sölden  Auftritt.</p>
+        </div>
+        <div class="column" style="width: 50;">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/testFest/TestFest_Farben.svg',
+            	'Test Fest Farben',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="illu-section" class="project-section small-padding no-padding-bottom has-background-image">
+    <div class="slot start">
+        <p class="side-note">Illustrations</p>
+    </div>
+    <div class="content"></div>
+    <div class="slot end"></div>
+    <?php new Image(
+    	null,
+    	'background-image',
+    	'/content/resources/media/testFest/TestFest_Illu_Skibrille.svg',
+    	'Test Fest Illustration Skibrille',
+    	true
+    ); ?>
+</section>
+<section class="project-section small-padding">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column" style="width: 28%;">
+            <h2>Illustrations</h2>
+            <p>Lockere, coole Scribbles werden als Muster, einzelne Illus oder aber sehr groß, abstrakt skaliert im Hintergrund verwendet.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section full-width-new no-padding-top">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/testFest/TestFest_Illu_Uebersicht.svg',
+        	'Test Fest Illustrationen Übersicht',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section overhang-right">
+    <div class="slot start">
+        <p class="side-note">Print – Folder</p>
+    </div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/testFest/TestFest_Print_Karte.png',
+        	'Test Fest Folder',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="poster-section" class="project-section no-padding-top" style="align-items: center;">
+    <div class="slot start">
+        <p class="side-note">Print – Plakat</p>
+    </div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/testFest/TestFest_Print_Plakat.png',
+        	'Test Fest Plakat',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section full-width-new no-padding-bottom no-padding-top bg-col-light-blue">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/testFest/TestFest_Illu_Uebersicht_schmal.svg',
+        	'Test Fest Illustrationen Übersicht',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-light-blue small-padding">
+    <div class="slot start"></div>
+    <div class="content" style="align-items: flex-start;">
+        <h2>Social Media</h2>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-light-blue no-padding-top large-padding-bottom" style="align-items: flex-end;">
+    <div class="slot start">
+        <p class="side-note">Social Media</p>
+    </div>
+    <div class="content">
+        <div class="column" style="width: 27.5%;">
+            <?php new Image(
+            	'shopping-event-image',
+            	null,
+            	'/content/resources/media/testFest/SOEL_ShoppingEvent_InstagramStory_Gewinn_Momente_1.jpg',
+            	'Test Fest Shopping Event',
+            	true
+            ); ?>
+        </div>
+        <div class="column" style="width: 72.5%;">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/testFest/TestFest_Carousel_mockup.png',
+            	'Test Fest Social Media Feed',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-light-blue small-padding">
+    <div class="slot start"></div>
+    <div class="content" style="align-items: flex-start;">
+        <h2>Anwendungen</h2>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="bag-section" class="project-section no-padding-top no-padding-bottom" style="align-items: center;">
+    <div class="slot start">
+        <p class="side-note">Design in use</p>
+    </div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/testFest/SOEL_TestFest_Taschen.png',
+        	'Test Fest Jutebeutel',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="mug-section" class="project-section bg-col-anthrazit overhang-right no-padding-top" style="align-items: center;">
+    <div class="slot start"></div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/testFest/SOEL_TestFest_Merch.jpg',
+        	'Test Fest Mugs',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="outro-section" class="project-section bg-col-anthrazit full-width-new no-padding-bottom" style="align-items: center;">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/testFest/TestFest_Tram.jpg',
+        	'Test Fest Tram-Beklebung',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
 </section>
