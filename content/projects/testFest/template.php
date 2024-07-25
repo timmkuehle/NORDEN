b9ad966c12ee6be5992ae308f961c9f8fe72ba84
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-25 13:11:32 +0200

    Added initial Test Fest project contents

--- content/projects/testFest/template.php
+++ content/projects/testFest/template.php
@@ -1,1 +1,37 @@
-<h1>Illustrations</h1>
+<section class="project-section full-width-new no-padding-top small-padding">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/testFest/TestFest_Header.jpg'
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section small-padding">
+    <div class="slot start"></div>
+    <div class="content" style="justify-content: flex-end;">
+        <div class="column" style="width: 60%; text-align: right;">
+            <p class="text-large bold uppercase">Shoppen, Testen, Ausprobieren</p>
+            <p class="text-large">Zahlreiche Marken präsentieren beim TEST FEST die neuesten Trends, während abwechslungs-reiche Side-Events mit heimischer Kulinarik, feinsten Beats und einer Afterparty der Extra-klasse Lust auf den neuen Skiwinter machen.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section no-padding-top small-padding">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column" style="width: 35%;">
+            <p>It's A Match: Teste & Finde dein neues Ski-Equipment. An zwei Wochenenden im November eröffnet Sölden das vermutlich variantenreichste Shopping-Angebot im Alpenraum. Sportfans kommen in den Genuss von ausgiebigen Testmöglichkeiten, einer ungeahnten Markenvielfalt sowie einem abwechslungsreichen Rahmenprogramm mit attraktiven Gewinnspielen.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="services">
+	<p class="service">Event Branding</p>
+	<p class="service">Corporate Design</p>
+	<p class="service">Branding & Implementierung</p>
+	<p class="service">Online & Offline Kommunikationskonzept</p>
+	<p class="service">Social Media</p>
+</section>
