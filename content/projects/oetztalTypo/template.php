1387904a29652e59de8d9c02314450d9c3da204a
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-25 16:27:22 +0200

    Finished Oetztal Typo project template

---
+++ content/projects/oetztalTypo/template.php
@@ -0,0 +1,204 @@
+<section class="project-section full-width-new no-padding-top">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Video(
+        	null,
+        	'/content/resources/media/oetztalTypo/01_Header_Video.mp4',
+        	'16/9',
+        	'/content/resources/media/oetztalTypo/01_Header_Video_Still.jpg',
+        	'Ötztal Font',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-dark-red">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column" style="width: 60%;">
+            <p class="text-large col-white">Im Falle des Ötztals soll sich die Schrift, als Ausdruck von Sprache, auf die Eigenständigkeit und Eigenwilligkeit des Ötztaler Dialekts beziehen. Schließlich gehört die Ötztaler Mundart zum Kulturerbe der UNESCO.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-dark-red">
+    <div class="slot start"></div>
+    <div class="content" style="align-items: flex-start;">
+        <div class="column" style="width: 58%;">
+            <div class="soelden-text-block">
+                <h2>Ein Corporate Font für das Ötztal</h2>
+                <h3>Leitidee:</h3>
+                <p class="col-white">In einer immer komplexer erscheinenden Welt ist es für Marken wichtiger denn je, eine klare Markenidentität zu schaffen. Kunden, Besucher und Fans sollten jederzeit in der Lage sein, alles zu erkennen, was die Marke ausmacht. Sowohl in Printmedien als auch in digitalen Medien. Einer der effektivsten Wege um dies zu erreichen, ist eine konstante, nach Maß gefertigte Schrift. Ein sogenannter Corporate Font.</p>
+                <p class="col-white">Die Eigenwilligkeit der Ötztaler Mundart spiegelt sich in geänderten Punktierungen, modifizierte Buchstaben und spezifische Ligaturen und Sonderzeichen wider. Ausgangsschrift für den Corporate Font des Ötztals war die „Adelle“ des Fontbüros „TypeTogether“.</p>
+                <p class="col-white">— Font creation in cooperation with <span class="col-gold">TypeTogether</span></p>
+            </div>
+        </div>
+        <div class="column" style="width: 42%;">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/oetztalTypo/01_LeitideeBild.JPG',
+            	'Ötztal Font Leitidee',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column">
+            <?php new Video(
+            	null,
+            	'/content/resources/media/oetztalTypo/OETZT_Umlaute_Anima.mp4',
+            	'1/1',
+            	'/content/resources/media/oetztalTypo/OETZT_Umlaute_Anima_Still.jpg',
+            	'Ötztal Font',
+            	true,
+            	true,
+            	true,
+            	true,
+            	false
+            ); ?>
+        </div>
+        <div class="column">
+            <h3>Punktierung:</h3>
+            <p>Die Individualisierung der Schrift fand ihren Ausgangspunkt in den signifikanten „Ö“-Strichen des Ötztal-Logos. Das „Ö“ spielt im Ötztaler Dialekt ohnehin eine ganz besondere Rolle. So wird vieles statt mit einem „O“ des Öfteren mit einem „Ö“ gesprochen und nicht selten wird im Ötztal zum Satzende ein „Ö“ angehängt. Dieser Dialekt mit Ecken und Kanten zeigt sich auch optisch: in einer eckigen Punktierung.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="modification-section" class="project-section no-padding-top">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/oetztalTypo/Modifizierung_A_1.svg',
+            	'Ötztal Font Modifizierungen',
+            	true
+            ); ?>
+            <?php new Image(
+            	null,
+            	'overlay',
+            	'/content/resources/media/oetztalTypo/Modifizierung_A_2.svg',
+            	null,
+            	true
+            ); ?>
+        </div>
+        <div class="column">
+            <h3>Modifizierungen:</h3>
+            <p>Um die Schrift eigenständig und unverwechselbar zu machen, wurden einige Buchstaben modifiziert und neu gezeichnet.</p>
+            <p>Die akustische Einzigartigkeit des Ötztaler Dialekts wird im Schriftbild auch optisch zu etwas ganz Besonderem. Dafür wurden verschiedene Ligaturen entwickelt, die die Verschmelzung zweier Buchstaben zu einer Glyphe zeigen.</p>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section full-width-new small-padding">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile">
+        <?php new Video(
+        	null,
+        	'/content/resources/media/oetztalTypo/OETZT_Ligaturen.mp4',
+        	'1920/700',
+        	'/content/resources/media/oetztalTypo/OETZT_Ligaturen_Still.jpg',
+        	'Ötztal Font Ligaturen',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-dark-red" style="align-items: flex-end;">
+    <div class="slot start"></div>
+    <div class="content">
+        <div class="column" style="width: 60%;">
+            <div class="soelden-text-block">
+                <h3>Typo Animations Konzept:</h3>
+                <p class="col-white">Die Individualisierung der Schrift fand ihren Ausgangspunkt in den signifikanten „Ö“-Strichen des Ötztal-Logos. Das „Ö“ spielt im Ötztaler Dialekt ohnehin eine ganz besondere Rolle. So wird vieles statt mit einem „O“ des Öfteren mit einem „Ö“ gesprochen und nicht selten wird im Ötztal zum Satzende ein „Ö“ angehängt. Dieser Dialekt mit Ecken und Kanten zeigt sich auch optisch: in einer eckigen Punktierung.</p>
+            </div>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="typo-animation-section" class="project-section bg-col-dark-red" style="align-items: flex-end;">
+    <div class="slot start">
+        <p class="side-note">Typo Animation</p>
+    </div>
+    <div class="content">
+        <?php new Video(
+        	null,
+        	'/content/resources/media/oetztalTypo/OETZT_Typo_Anima_Konzept.mp4',
+        	'16/9',
+        	'/content/resources/media/oetztalTypo/OETZT_Typo_Anima_Konzept_Still.jpg',
+        	'Ötztal Typo Animation Konzept',
+        	true,
+        	true,
+        	true,
+        	true,
+        	false
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section id="big-a-section" class="project-section full-width-new no-padding-top has-background-image">
+    <div class="slot start"></div>
+    <div class="content no-inline-padding-mobile" style="align-items: flex-start;">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/oetztalTypo/06_OETZT_TypoA_groß.svg',
+        	'Ötztal Font A',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+    <?php new Image(
+    	null,
+    	'background-image',
+    	'/content/resources/media/oetztalTypo/07_OETZT_Claim.svg',
+    	'Ötztal bewegt dich',
+    	true
+    ); ?>
+</section>
+<section id="overview-section" class="project-section">
+    <div class="slot start"></div>
+    <div class="content" style="align-items: stretch;">
+        <div class="column" style="width: 12%;">
+            <p class="col-red">Customized Headline Font</p>
+            <p class="col-red">Ötztal</p>
+        </div>
+        <div class="column" style="width: 88%;">
+            <?php new Image(
+            	null,
+            	null,
+            	'/content/resources/media/oetztalTypo/01_Header.svg',
+            	'Ötztal Font Header',
+            	true
+            ); ?>
+        </div>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section bg-col-dark-red large-padding-top large-padding-bottom">
+    <div class="slot start"></div>
+    <div class="content">
+        <?php new Image(
+        	null,
+        	null,
+        	'/content/resources/media/oetztalTypo/OETZT_ganzes_Alphabet.svg',
+        	'Ötztal Font Alphabet',
+        	true
+        ); ?>
+    </div>
+    <div class="slot end"></div>
+</section>
