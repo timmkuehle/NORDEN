c8a45969211a379e00dcf0608029e7c3c69133ce
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-07-26 15:45:20 +0200

    Added privacy page

--- content/pages/privacy/template.php
+++ content/pages/privacy/template.php
@@ -1,1 +1,27 @@
-<h1>Datenschutz</h1>
+<section class="project-section small-padding">
+    <div class="slot start">
+        <p class="side-note">Informartionen zum Datenschutz</p>
+    </div>
+    <div class="content">
+        <h1>Datenschutzerklärung</h1>
+        <p>Der Schutz Ihrer persönlichen Daten ist uns ein besonderes Anliegen. Wir verarbeiten Ihre Daten daher ausschließlich auf Grundlage der gesetzlichen Bestimmungen (DSGVO, TKG 2003). In diesen Datenschutzinformationen informieren wir Sie über die wichtigsten Aspekte der Datenverarbeitung im Rahmen unserer Website.</p>
+        <h2>Kontakt mit uns</h2>
+        <p>Wenn Sie per E-Mail Kontakt mit uns aufnehmen, werden Ihre angegebenen Daten zwecks Bearbeitung der Anfrage und für den Fall von Anschlussfragen sechs Monate bei uns gespeichert. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.</p>
+        <h2>Ihre Rechte</h2>
+        <p>Ihnen stehen grundsätzlich die Rechte auf Auskunft, Berichtigung, Löschung, Einschränkung, Datenübertragbarkeit, Widerruf und Widerspruch zu. Wenn Sie glauben, dass die Verarbeitung Ihrer Daten gegen das Datenschutzrecht verstößt oder Ihre datenschutzrechtlichen Ansprüche sonst in einer Weise verletzt worden sind, können Sie sich bei der Aufsichtsbehörde beschweren. In Österreich ist dies die Datenschutzbehörde.</p>
+    </div>
+    <div class="slot end"></div>
+</section>
+<section class="project-section small-padding">
+    <div class="slot start">
+        <p class="side-note">Kontakt</p>
+    </div>
+    <div class="content">
+        <p>Sie erreichen uns unter folgenden Kontaktdaten:</p>
+        <h2>NORDEN GmbH –</h2>
+        <h3>visuelle Kommunikation, Markenentwicklung & Beratung</h3>
+        <p>Kaiser-Franz-Joseph-Str. 10<br>6020 Innsbruck</p>
+        <a href="mailto:mail@norden.co">mail@norden.co</a>
+    </div>
+    <div class="slot end"></div>
+</section>
