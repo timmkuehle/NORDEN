d6f3131d65e1f5fd9c541ae0bae452705c3b33f2
Tim Moritz Kühle <timmoritz.kuehle@outlook.com>
2025-07-08 15:05:00 +0200

    Adjusted ProjectsPreview component layout

--- components/ProjectsPreview/ProjectsPreview.php
+++ components/ProjectsPreview/ProjectsPreview.php
@@ -1,55 +1,45 @@
 <?php
 
-class ProjectsPreview extends PHTMLComponent {
-	private ProjectModel $model;
-	private array $projects;
+class ProjectsPreview extends \PHTMLComponent {
+    private \ProjectModel $model;
+    private array $projects;
 
-	public function __construct(
-		?string $id,
-		?string $class_name,
-		array $project_slugs = []
-	) {
-		$this->model = new ProjectModel();
+    public function __construct(
+        ?string $id,
+        ?string $class_name,
+        array $project_slugs = []
+    ) {
+        $this->model = new \ProjectModel();
 
-		$this->projects = $this->model->getProjects($project_slugs);
+        $this->projects = $this->model->getProjects($project_slugs);
 
-		parent::__construct($id, $class_name);
-	}
+        parent::__construct($id, $class_name);
+    }
 
-	protected function render() {
-		?>
+    protected function render() {
+        ?>
         <section <?php $this->renderHTMLAttributes(); ?>>
-			<p class="text-style-subline vertical-text color-primary">Our Projects</p>
 			<section class="projects">
 				<?php foreach ($this->projects ?? [] as $index => $project): ?>
 					<article class="project">
-						<a id="project-<?php echo $index + 1; ?>" class="project-link"
-						href="<?php echo BASE_URL . $project['slug']; ?>">
-							<header class="project-header">
-								<h3 class="project-category"><?php echo $project['category'] ??
-        	'Uncategorized'; ?></h3>
-							</header>
-							<?php new Image(
-       	null,
-       	'project-thumbnail',
-       	$project['thumbnail'],
-       	'Prokjekt ansehen: ' . $project['title'],
-       	true
-       ); ?>
+						<a id="project-<?= $index + 1; ?>" class="project-link"
+						href="<?= BASE_URL . $project['slug']; ?>">
+							<?php new \Image(
+							    null,
+							    'project-thumbnail',
+							    $project['thumbnail'],
+							    'Prokjekt ansehen: ' . $project['title'],
+							    true
+							); ?>
+							<footer class="project-footer">
+								<h2 class="project-title"><?= $project['preview_title'] ?? $project['title']; ?></h2>
+								<p class="project-category"><?= $project['category'] ?? 'Uncategorized'; ?></p>
+							</footer>
 						</a>
 					</article>
 				<?php endforeach; ?>
-				<div class="project-archive-button-container">
-					<?php new Button(
-     	null,
-     	'project-archive-button no-min-width',
-     	'Alle Projekte',
-     	'/projekte',
-     	'secondary'
-     ); ?>
-				</div>
 			</section>
         </section>
 	<?php
-	}
+    }
 }
