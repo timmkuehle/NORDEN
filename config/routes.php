470e4238f0573e64ab3fbb883afa8b5d12724b44
Tim Moritz Kühle <timmoritz.kuehle@outlook.com>
2025-07-08 17:56:20 +0200

    Renamed Projekte to Work in routes, MenuModel, PageModel and home template link

--- config/routes.php
+++ config/routes.php
@@ -1,45 +1,47 @@
 <?php
 
-Router::addRoute('/', 'PageController');
+declare(strict_types=1);
 
-Router::addRoute('/projekte', 'PageController');
+\Router::addRoute('/', 'PageController');
 
-Router::addRoute('/agentur', 'PageController');
+\Router::addRoute('/work', 'PageController');
 
-Router::addRoute('/jobs', 'PageController');
+\Router::addRoute('/agentur', 'PageController');
 
-Router::addRoute('/kontakt', 'PageController');
+\Router::addRoute('/jobs', 'PageController');
 
-Router::addRoute('/projekte/soelden', 'ProjectController');
+\Router::addRoute('/kontakt', 'PageController');
 
-Router::addRoute('/projekte/soelden/typografie', 'ProjectController');
+\Router::addRoute('/projekte/soelden', 'ProjectController');
 
-Router::addRoute(
-	'/projekte/soelden/electric-mountain-festival',
-	'ProjectController'
+\Router::addRoute('/projekte/soelden/typografie', 'ProjectController');
+
+\Router::addRoute(
+    '/projekte/soelden/electric-mountain-festival',
+    'ProjectController'
 );
 
-Router::addRoute('/projekte/aranea', 'ProjectController');
+\Router::addRoute('/projekte/aranea', 'ProjectController');
 
-Router::addRoute(
-	'/projekte/soelden/oetztaler-radmarathon',
-	'ProjectController'
+\Router::addRoute(
+    '/projekte/soelden/oetztaler-radmarathon',
+    'ProjectController'
 );
 
-Router::addRoute('/projekte/gurgl', 'ProjectController');
+\Router::addRoute('/projekte/gurgl', 'ProjectController');
 
-Router::addRoute('/projekte/alpbachtal', 'ProjectController');
+\Router::addRoute('/projekte/alpbachtal', 'ProjectController');
 
-Router::addRoute('/projekte/seefeld', 'ProjectController');
+\Router::addRoute('/projekte/seefeld', 'ProjectController');
 
-Router::addRoute('/projekte/holtmeyer', 'ProjectController');
+\Router::addRoute('/projekte/holtmeyer', 'ProjectController');
 
-Router::addRoute('/projekte/soelden/test-fest', 'ProjectController');
+\Router::addRoute('/projekte/soelden/test-fest', 'ProjectController');
 
-Router::addRoute('/projekte/oetztal/font', 'ProjectController');
+\Router::addRoute('/projekte/oetztal/font', 'ProjectController');
 
-Router::addRoute('/impressum', 'PageController');
+\Router::addRoute('/impressum', 'PageController');
 
-Router::addRoute('/datenschutz', 'PageController');
+\Router::addRoute('/datenschutz', 'PageController');
 
-Router::addRoute('/allgemeine-geschaeftsbedingungen', 'PageController');
+\Router::addRoute('/allgemeine-geschaeftsbedingungen', 'PageController');
--- models/MenuModel.php
+++ models/MenuModel.php
@@ -10,67 +10,67 @@ declare(strict_types=1);
  * @property array $menus Holds information arrays for all existing menus
  */
 class MenuModel {
-	private array $menus = [
-		'header_menu' => [
-			[
-				'title' => 'Projekte',
-				'link' => '/projekte'
-			],
-			[
-				'title' => 'Agentur',
-				'link' => '/agentur'
-			],
-			[
-				'title' => 'Jobs',
-				'link' => '/jobs'
-			]
-		],
-		'contact_menu' => [
-			[
-				'title' => 'Kontakt',
-				'link' => 'kontakt'
-			]
-		],
-		'footer_menu' => [
-			[
-				'title' => 'Impressum',
-				'link' => '/impressum'
-			],
-			[
-				'title' => 'Datenschutz',
-				'link' => '/datenschutz'
-			],
-			[
-				'title' => 'AGB',
-				'link' => '/allgemeine-geschaeftsbedingungen'
-			]
-		]
-	];
+    private array $menus = [
+        'header_menu' => [
+            [
+                'title' => 'Work',
+                'link' => '/work'
+            ],
+            [
+                'title' => 'Agentur',
+                'link' => '/agentur'
+            ],
+            [
+                'title' => 'Jobs',
+                'link' => '/jobs'
+            ]
+        ],
+        'contact_menu' => [
+            [
+                'title' => 'Kontakt',
+                'link' => 'kontakt'
+            ]
+        ],
+        'footer_menu' => [
+            [
+                'title' => 'Impressum',
+                'link' => '/impressum'
+            ],
+            [
+                'title' => 'Datenschutz',
+                'link' => '/datenschutz'
+            ],
+            [
+                'title' => 'AGB',
+                'link' => '/allgemeine-geschaeftsbedingungen'
+            ]
+        ]
+    ];
 
-	/**
-	 * Menu sanitization method
-	 *
-	 * Sanitizes all links of requested menu
-	 *
-	 * @param array $raw_menu Menu to sanitize
-	 * @return array Sanitized menu
-	 */
-	private function sanitizeMenu(array $raw_menu): array {
-		return array_map(
-			fn($menu) => array_replace($menu, [
-				'link' => sanitize_uri($menu['link'])
-			]),
-			$raw_menu
-		);
-	}
+    /**
+     * Menu sanitization method
+     *
+     * Sanitizes all links of requested menu
+     *
+     * @param array $raw_menu Menu to sanitize
+     * @return array Sanitized menu
+     */
+    private function sanitizeMenu(array $raw_menu): array {
+        return array_map(
+            fn($menu) => array_replace($menu, [
+                'link' => sanitize_uri($menu['link'])
+            ]),
+            $raw_menu
+        );
+    }
 
-	/**
-	 * Menu getter
-	 *
-	 * @param string $slug Slug of requested menu
-	 * @return array Menu information, requested by menu controller
-	 */
-	public function getMenu(string $slug): array {
-		return $this->sanitizeMenu($this->menus[$slug]);
-	}
+    /**
+     * Menu getter
+     *
+     * @param string $slug Slug of requested menu
+     * @return array Menu information, requested by menu controller
+     */
+    public function getMenu(string $slug): array {
+        return $this->sanitizeMenu($this->menus[$slug]);
+    }
 }
