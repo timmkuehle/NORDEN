a6db5cd67e6a0909803a2a82e26c98fc3596a591
Tino <tino.schlenker@outlook.com>
2026-04-27 21:42:38 +0200

    kaiserweis kids done

--- content/projects/kaiserweisKids/template.php
+++ content/projects/kaiserweisKids/template.php
@@ -1,7 +1,7 @@
 <?php
-$seoTitle = 'Florian Manz Branding, Logodesign & Corporate Design | NORDEN';
+$seoTitle = 'Kaiserweis Kids Käse Boxen Packaging Design | NORDEN';
 $seoDescription =
-	'Für den Dokumentarfotografen Florian Manz entwickelte NORDEN ein modulares Branding mit Logodesign, Typografie und Geschäftsausstattung – reduziert, klar und dokumentarisch prägnant.';
+	'Für die Käsregion Kaiserweis entwickelte NORDEN zwei sehr unterschiedlich Käse Boxen. Zum einen, eine farbenfrohe für Kinder, mit Fokus auf Spiel und Spaß. Auf der anderen Seite eine traditionelle Holzkiste für Erwachsene. Regionalität spielt dabei eine große Rolle.';
 
 if (isset($NORDEN_TEMPLATE_META_ONLY) && $NORDEN_TEMPLATE_META_ONLY) {
 	$NORDEN_TEMPLATE_META = [
@@ -71,11 +71,131 @@ if (isset($NORDEN_TEMPLATE_META_ONLY) && $NORDEN_TEMPLATE_META_ONLY) {
 		</div>
 	</div>
     <?php new Image(
+    	null,
+    	'illustration',
+    	'/content/resources/media/kaiserweis/kids-box/03_Grafikelemente/01_KUUSK_Käseboxen_Maus-Illu.svg',
+    	'Kaiserweis Kids Hero',
+    	true
+    ); ?>
+</section>
+
+<section class="project-section section-6 bg-col-white pb-0 pt-0">
+<?php new Image(
 	null,
-	'illustration',
-	'/content/resources/media/kaiserweis/kids-box/03_Grafikelemente/01_KUUSK_Käseboxen_Maus-Illu.svg',
-	'Kaiserweis Kids Hero',
+	'svg-banner',
+	'/content/resources/media/kaiserweis/kids-box/03_Grafikelemente/02_KUUSK_Kidsbox_Typo.svg',
+	'Cheese Typography',
 	true
 ); ?>
 </section>
+
+<section class="project-section section-7 bg-col-white pb-0 pt-0">
+<?php new Image(
+	null,
+	'look-and-feel',
+	'/content/resources/media/kaiserweis/kids-box/03_Grafikelemente/03_KUUSK_LookFeel_Kidsbox.png',
+	'Look and Feel',
+	true
+); ?>
+</section>
+
+<section class="project-section section-8 bg-col-white">
+	<div class="row centered has-padding-lr">
+		<div class="col s16 m10 l9 xl8">
+            <h4 class="small-title">VON KINDERN UND FÜR KINDER</h3>
+			<p class="is-text is-indented">Wir durften die Käsregion Kaiserweis bei einem spannenden Packaging Projekt unterstützen. Und dabei zwei sehr unterschiedlich Kastl entwickeln. Zum einen, eine farbenfrohe für Kinder, mit Fokus auf Spiel und Spaß. Wichtig war bei zweitem vor allem der Austausch mit Kindern und deren Feedback, sowie das ein-beziehen der künstlerischen Arbeiten in das endgültige Produkt.</p>
+		</div>
+	</div>
+</section>
+
+<section class="project-section section-9 bg-col-white pt-0">
+	<div class="row centered has-padding-lr">
+		<div class="col s16 m14 l12">
+		<?php new Image(
+  	null,
+  	'look-and-feel',
+  	'/content/resources/media/kaiserweis/kids-box/03_Grafikelemente/04_KUUS_Kaiserweis__Illustrationen_kisBox.png',
+  	'Look and Feel',
+  	true
+  ); ?>		
+		</div>
+	</div>
+</section>
+
+<section class="project-section section-10 bg-col-white">
+	<div class="row centered has-padding-lr">
+		<div class="col s16 m10 l9 xl8">
+            <h4 class="small-title">VON KINDERN UND FÜR KINDER</h3>
+			<p class="is-text is-indented">Wir durften die Käsregion Kaiserweis bei einem spannenden Packaging Projekt unterstützen. Und dabei zwei sehr unterschiedlich Kastl entwickeln. Zum einen, eine farbenfrohe für Kinder, mit Fokus auf Spiel und Spaß. Wichtig war bei zweitem vor allem der Austausch mit Kindern und deren Feedback, sowie das ein-beziehen der künstlerischen Arbeiten in das endgültige Produkt.</p>
+		</div>
+	</div>
+</section>
+
+<section class="project-section section-11 bg-col-white pt-0">
+	<div class="row centered has-padding-lr">
+		<div class="col s16">
+		<?php new Image(
+  	null,
+  	'mockup-1',
+  	'/content/resources/media/kaiserweis/kids-box/04_Mockup/01_KidsBox_Mockup_1.jpg',
+  	'Mockup 1',
+  	true
+  ); ?>
+		<?php new Image(
+  	null,
+  	'mockup-2',
+  	'/content/resources/media/kaiserweis/kids-box/04_Mockup/02_KidsBox_Illus.jpeg',
+  	'Mockup 2',
+  	true
+  ); ?>		
+			<?php new Image(
+   	null,
+   	'mockup-2',
+   	'/content/resources/media/kaiserweis/kids-box/04_Mockup/03_KidsBox_Mockup.jpg',
+   	'Mockup 2',
+   	true
+   ); ?>	
+
+		</div>
+	
+	
+	</div>
+	<div class="row has-padding-lr flex-end">
+	<div class="col s8 flex-end-col mb-0">
+		<?php new Image(
+  	null,
+  	'mouse-svg',
+  	'/content/resources/media/kaiserweis/kids-box/03_Grafikelemente/01_KUUSK_Käseboxen_Maus-Illu.svg',
+  	'Mouse Illustration',
+  	true
+  ); ?>
+		<?php new Image(
+  	null,
+  	'cheese-photo mb-0',
+  	'/content/resources/media/kaiserweis/kids-box/04_Mockup/05_pexels-cheese_pic.jpg',
+  	'Cheese Photo',
+  	true
+  ); ?>
+		</div>
+		<div class="col s8 mb-0">
+		<?php new Image(
+  	null,
+  	'kidsbox-photo mb-0',
+  	'/content/resources/media/kaiserweis/kids-box/04_Mockup/04_KidsBox_Foto_2.jpeg',
+  	'Kidsbox Photo',
+  	true
+  ); ?>
+		</div>
+	</div>
+</section>
+<div class="row has-padding-lr btn-row">
+	<div class="col s16 m8 flex-end-col">
+	<?php new Button(
+ 	null,
+ 	'project-button',
+ 	'Zum Premium Käse Kisterl',
+ 	'/projekte/kaiserweis-kids'
+ ); ?>
+	</div>
+</div>
 </div>
